cbf60fc6232a6ce3b683146448df98c47a0ea1aa
David Andreoletti <david.andreoletti.dev@gmail.com>
2013-09-10 14:01:04 +0800

    hgrc: Added custom config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+
+DOTFILESSETUPTYPE="perso" 
+while getopts s:h flag; do
+  case $flag in
+    s)
+      DOTFILESSETUPTYPE="$OPTARG";
+      ;;
+    h)
+      echo "Help: bootstrap.sh [options]";
+      echo " -s value\t Setup type to install. Valid values (case sensitive): "
+      echo "    work  - For computers at work."
+      echo "    perso - For computers at home."
+      echo " -h This help."
+      ;;
+    ?)
+      echo "Unsupported option. Exit."
+      exit;
+      ;;
+  esac
+done
+
+shift $(( OPTIND - 1 ));
+
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.rst" -av . ~
+	rsync --exclude ".git/" --exclude "custom/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.rst" -av . ~
+        local p="custom/${DOTFILESSETUPTYPE}"
+	cd "${p}" && rsync  -av . ~ && cd - || echo "Cannot find ${p}. Exit." && exit
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
