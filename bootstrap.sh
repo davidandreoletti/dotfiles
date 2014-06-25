bbe082db5069f361514ea1d223fe6064fd905057
David Andreoletti <david.andreoletti.dev@gmail.com>
2014-06-25 11:04:17 +0800

    bootstrap: Added support for selecting default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 FORCE_INSTALL=false
 DOTFILESSETUPTYPE="perso" 
-while getopts fs:h flag; do
+SHELLTYPE="bash"
+while getopts fs:t:h flag; do
   case $flag in
     f)
       FORCE_INSTALL=true;
@@ -9,12 +10,18 @@ while getopts fs:h flag; do
     s)
       DOTFILESSETUPTYPE="$OPTARG";
       ;;
+    t)
+      SHELLTYPE="$OPTARG";
+      ;;
     h)
       echo "Help: bootstrap.sh [options]";
       echo " -f         Force install."
       echo " -s value\t Setup type to install. Valid values (case sensitive): "
       echo "    work  - For computers at work."
-      echo "    perso - For computers at home."
+      echo "    perso - For computers at home (default)."
+      echo " -t value\t Shell type to use by default. Valid values (case sensitive): "
+      echo "    bash - Bash shell (default)"
+      echo "    zsh  - ZSH shell"
       echo " -h This help."
       ;;
     ?)
@@ -30,6 +37,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
 	chmod -R 700 .
+    echo -n "$USER's "; chsh -s $(which $SHELLTYPE)
 	rsync --exclude ".git/" --exclude "custom/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.rst" -av . ~
         local p="custom/${DOTFILESSETUPTYPE}"
 	cd "${p}" && rsync  -av . ~ && cd - || echo "Cannot find ${p}. Exit." && exit
