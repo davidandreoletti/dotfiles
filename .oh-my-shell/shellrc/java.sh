92d500fbcb42e2af21cedb278b8b11ee85a44fb5
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-01-26 21:20:42 +0800

    jenv: Most recent java version is picked as default java version for the current shell.

--- .oh-my-shell/shellrc/java.sh
+++ .oh-my-shell/shellrc/java.sh
@@ -6,5 +6,9 @@ if [[ "$os_type_name" == "macosx" ]]; then
     find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\" > /dev/null 2>&1" \;
     #Export JAVA_HOME with jenv export command
     jenv enable-plugin export > /dev/null  2>&1; 
+    # By default, jenv uses the most recent version
+    mostRecentJavaVersion=`jenv versions | sort | head -n1 | cut -f1`; 
+    mostRecentJavaVersion=`echo "$mostRecentJavaVersion" | sed 's/ //g'`;
+    echo "$mostRecentJavaVersion" > $HOME/.java-version
 fi
 unset os_type_name
