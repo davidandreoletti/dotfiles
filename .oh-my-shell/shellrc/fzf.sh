1cbe94e9058876ac8bcf76e609d6a4ae222d20e1
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-01-29 10:57:49 +0800

    shellrc: Initial fzf/ripgrep configuration

---
+++ .oh-my-shell/shellrc/fzf.sh
@@ -0,0 +1,19 @@
+# Trigger sequence for fuzzy completion
+# Eg: 
+# - vim **<TAB> 
+# - kill -9 <TAB>
+# - ssh **<TAB>
+# - For a complete list of commands supported: complete | grep _fzf
+export FZF_COMPLETION_TRIGGER='**'
+
+# Default fzf options
+# --height fzf ui height, relative to containing shell/window
+# --layout=reverse Top down listing
+# --multi Select multiple files, instead of just one. Use <TAB> to select each file
+# --no-mouse Power user mode :P
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --inline-info --multi --no-mouse'
+
+# Fzf uses ripgrep as default command to search for files 
+# FIXME: Fallback to find if rg is not available
+export FZF_DEFAULT_COMMAND='rg --files 2> /dev/null'
+
---
+++ .oh-my-shell/shellrc/ripgrep.sh
@@ -0,0 +1,4 @@
+# ripgrep default configuration file location
+# - by default no configuration file is read
+export RIPGREP_CONFIG_PATH="$HOME/.ripgrep.conf"
+
