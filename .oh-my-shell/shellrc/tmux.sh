12c1849a7364d534b1469e33073e95b9ad5a7c00
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-01-31 22:45:02 +0800

    shellrc: tmux is now aliased. Aliases created a named session based on directory the tmux session was openned on.

--- .oh-my-shell/shellrc/tmux.sh
+++ .oh-my-shell/shellrc/tmux.sh
@@ -133,3 +133,10 @@ tmux_android_hide() {
     tmux_show_window "$sessionName" "adblog"
 }
 
+tmux_session_get_or_create() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then 
+     tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
