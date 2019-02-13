9f521663a1f35f8bdb7c2422901425046d83ce16
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-02-13 16:38:50 +0800

    zsh: Ignored common noise command from being saved in the history (clear, history, pwd, etc).

--- .oh-my-shell/bash/history/default.sh
+++ .oh-my-shell/bash/history/default.sh
@@ -1,5 +1,8 @@
 # Shell history
 shopt -s histappend
+
+# Make new shells get the history lines from all previous
+# shells instead of the default "last window closed" history.
 export PROMPT_COMMAND='history -a'
 HISTCONTROL=ignoreboth
 HISTSIZE=10000
@@ -7,3 +10,8 @@ HISTFILESIZE=$HISTSIZE
 HISTTIMEFORMAT='%F %T%t'
 HISTFILE=$HOME/.bash_history
 
+# Don't add certain commands to the history file.
+export HISTIGNORE="&:[bf]g:c:clear:history:exit:q:pwd:* --help"
+
+# Save all lines in a multiline command in the same history entry
+shopt -s cmdhist
