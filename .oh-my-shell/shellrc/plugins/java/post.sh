08eb583be57fc57e2cd2b678386aab294181e719
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-02-13 11:45:24 +0800

    java: Explicitly exported JAVA_HOME for GUI apps using macOs built-in java tooling.

--- .oh-my-shell/shellrc/plugins/java/post.sh
+++ .oh-my-shell/shellrc/plugins/java/post.sh
@@ -1,13 +1,17 @@
 if [[ "$OS_NAME" == "macosx" ]]; then
+	#Export JAVA_HOME for GUI/shell applications
+    launchctl setenv JAVA_HOME `/usr/libexec/java_home`
+
+    # Export "JAVA_HOME" for shell application only
+    # via jenv. Useful for custom development
     # Initialize jenv files
     eval "$(jenv init -)" 
     # Register Java SDKs
     find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\" > /dev/null 2>&1" \;
-    #Export JAVA_HOME with jenv export command
+    #jenv used to select a specific java version
     jenv enable-plugin export > /dev/null  2>&1; 
     # By default, jenv uses the most recent version
     mostRecentJavaVersion=`jenv versions | sort | head -n1 | cut -f1`; 
     mostRecentJavaVersion=`echo "$mostRecentJavaVersion" | sed 's/ //g'`;
     echo "$mostRecentJavaVersion" > $HOME/.java-version
 fi
-unset os_type_name
