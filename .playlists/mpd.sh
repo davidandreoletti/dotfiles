7489cd946cc9d570a948fef1abd7beadab8a405e
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-02-13 23:47:03 +0800

    Added "bin/music" binary to load musics from various sources and begin audio playback.

---
+++ .playlists/mpd.sh
@@ -0,0 +1,19 @@
+# Usage: mpd_loader.sh file.m3u file2.m3u ...
+
+for f in $*
+do
+    if [ -f $f ] && [ -r $f ]
+    then
+        all=$(echo "$(command cat $f | sort)")
+        youtube=$(echo -e "$all" | command grep "youtube.com")
+        remaining=$(command comm -23 <(echo -e "$all") <(echo -e "$youtube"))
+
+        # "remaining" urls can be used as is
+        remainingFd=<(echo -e "$remaining") \
+        # "youtube" urls must be contverted first
+        youtubeFd=<(echo -e "$youtube" | xargs youtube-dl -g )
+
+        # Print to stdout url to be loaded by mpc add or similar client
+        command cat $remainingFd $youtubeFd 
+    fi
+done
