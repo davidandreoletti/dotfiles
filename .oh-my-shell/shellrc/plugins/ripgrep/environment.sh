6d8a6d8981fc3b10e92b2473a750fb8344806a3c
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-02-13 14:33:20 +0800

    ripgrep: Fixed RIPGREP_CONFIG_PATH to read to the $HOME/.ripgreprc config file by default

--- .oh-my-shell/shellrc/plugins/ripgrep/environment.sh
+++ .oh-my-shell/shellrc/plugins/ripgrep/environment.sh
@@ -1,4 +1,3 @@
 # ripgrep default configuration file location
 # - by default no configuration file is read
-export RIPGREP_CONFIG_PATH="$HOME/.ripgrep.conf"
-
+export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
