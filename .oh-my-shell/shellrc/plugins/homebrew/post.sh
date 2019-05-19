03b3d3a9c8e21e19ca40a5c616d0765cb4f8e6f0
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-05-19 20:55:51 +0800

    brew: Upgrade brew non cask and casks

--- .oh-my-shell/shellrc/plugins/homebrew/post.sh
+++ .oh-my-shell/shellrc/plugins/homebrew/post.sh
@@ -1,8 +1,61 @@
 # Upgrade homebrew managed packages
 
-HOMEBREW_PACKAGES_UPDATED="/tmp/${USER}_homebrew_packages_upgrade"
+HOMEBREW_PACKAGES_UPDATED="/tmp/${USER}_homebrew_packages_upgraded"
 HOMEBREW_PACKAGES_UPGRADE_SCRIPT="/tmp/${USER}_homebrew_packages_upgrade.sh"
 HOMEBREW_PACKAGES_UPGRADE_LOG="/tmp/${USER}_homebrew_packages_upgrade.log"
+
+function upgradeBrewPackagesInBackground () {
+
+cat <<EOF > "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT"
+    set -x
+
+    brew update 2>&1;
+
+    # Uninstall previous non cask and cask package versions (to save on disk space over time)
+    # Note: brew cask cleanup now deprecated. brew cleanup must be used instead 
+    brew cleanup 2>&1;
+
+    # Upgrade non cask packages
+    # - upgrade non casks
+    brew upgrade 2>&1;
+
+    touch "$HOMEBREW_PACKAGES_UPDATED"
+    chmod 777 "$HOMEBREW_PACKAGES_UPDATED"
+    rm -f "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT"
+
+    set +x
+EOF
+
+bash "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT" > "$HOMEBREW_PACKAGES_UPGRADE_LOG" 2>&1 &
+
+}
+
+function upgradeBrewPackagesInForeground () {
+
+cat <<EOF > "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT"
+    weeknumber=\`date +%V\`;
+    reminder="\$weeknumber % 4"
+    if [ \`echo "\$reminder" | bc\` -eq 0 ];
+    then
+        echo "Brew cask packages last upgraded 4+ weeks ago. Upgrading now. Ctrl-C to cancel"
+        # Upgrade cask packages
+        # - upgrade cask with explicitly version
+        brew cask upgrade --force 2>&1;
+        # - upgrade cask with "lastest" version or "auto_updates"
+        brew cask upgrade --greedy --force 2>&1;
+    fi 
+EOF
+
+source "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT" | tee "$HOMEBREW_PACKAGES_UPGRADE_LOG"
+
+}
+
+function isCurrentUserHomebrewCellarDirectoryOwner () {
+	local fileOrDirPath="$1"
+	local resourceOwner="$(stat -c '%U' $fileOrDirPath)"
+	
+	[ "$USER" == "$resourceOwner" ]
+}
 
 # Force packages upgrades every 1d
 if [ -f "$HOMEBREW_PACKAGES_UPDATED" ];
@@ -19,36 +72,7 @@ fi
 # Upgrade packages when required
 if [ ! -f "$HOMEBREW_PACKAGES_UPDATED" ] && [ ! -f "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT" ];
 then
-
-cat <<EOF > "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT"
-    set -x
-
-    brew update 2>&1;
-
-    # Uninstall previous non cask and cask package versions (to save on disk space over time)
-    brew cleanup 2>&1;
-    # brew cask cleanup now depecrated. brew cleanup must be used instead 
-
-    # Upgrade non cask and cask brew packages
-    # - upgrade non casks
-    brew upgrade 2>&1;
-    # - upgrade cask with explicitly version
-    brew cask upgrade --force 2>&1;
-    # - upgrade cask with "lastest" version or "auto_updates"
-    #   These are upgraded every 4 weeks
-    weeknumber=\`date +%V\`;
-    reminder="\$weeknumber % 4"
-    [ \`echo "\$reminder" | bc\` -eq 0 ] && brew cask upgrade --greedy --force 2>&1;
-
-    touch "$HOMEBREW_PACKAGES_UPDATED"
-    chmod 777 "$HOMEBREW_PACKAGES_UPDATED"
-    rm -f "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT"
-
-    set +x
-EOF
-
-rm -f "$HOMEBREW_PACKAGES_UPGRADE_LOG" > /dev/null 2>&1
-
-bash "$HOMEBREW_PACKAGES_UPGRADE_SCRIPT" > "$HOMEBREW_PACKAGES_UPGRADE_LOG" 2>&1 &
-
+    rm -f "$HOMEBREW_PACKAGES_UPGRADE_LOG" > /dev/null 2>&1
+    isCurrentUserHomebrewCellarDirectoryOwner "$(brew --prefix)/Cellar" && upgradeBrewPackagesInForeground
+    isCurrentUserHomebrewCellarDirectoryOwner "$(brew --prefix)/Cellar" && upgradeBrewPackagesInBackground
 fi
