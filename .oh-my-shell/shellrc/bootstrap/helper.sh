252579a1e2dfe1b07d2b701d167b841cbccdfe8e
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-06-11 22:49:49 +0800

    shellrc: Added "disabled" shellrc plugin loading profiler.

--- .oh-my-shell/shellrc/bootstrap/helper.sh
+++ .oh-my-shell/shellrc/bootstrap/helper.sh
@@ -46,6 +46,12 @@ if_dir_exists() {
 # Usage: dot_plugin_if_exists PLUGIN_NAME
 dot_plugin_if_exists() {
     local pluginName="${1:-"none"}"
+
+    #set -x
+    #startTime=$(command date "+%s%3N")
+    #startTime="${startTime//[!0-9]/}"
+    #set +x
+
     export SHELLRC_CURRENT_PLUGIN_DIR="${SHELLRC_PLUGINS_DIR}/${pluginName}"
     if_dir_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/bin" && path_prepend "${SHELLRC_CURRENT_PLUGIN_DIR}/bin"
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/environment.sh"
@@ -55,6 +61,18 @@ dot_plugin_if_exists() {
     # FIXME: post.sh must be executed when all plugins have been initialized
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/post.sh"
     unset SHELLRC_CURRENT_PLUGIN_DIR 
+
+    #set -x
+    #endTime=$(command date "+%s%3N")
+    #endTime="${endTime//[!0-9]/}"
+    #runtime=$(echo "$endTime - $startTime" | bc)
+    #set +x
+    # Report plugins loading slower than 200 milliseconds
+    #if [[ $runtime -gt 200 ]];
+    #then
+    #    #echo "$startTime > $endTime"
+    #    echo "$runtime ms -> $pluginName"
+    #fi
 }
 
 # Usage: dot_current_shell_plugin_if_exists PLUGIN_NAME
