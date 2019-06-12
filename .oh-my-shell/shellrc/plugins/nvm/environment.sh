d8c687e723c0ff1f055210a49b3b7f51e7dbcaab
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-06-12 16:27:12 +0800

    shellrc: Reduced loading nvm plugin speed by ~3x.

--- .oh-my-shell/shellrc/plugins/nvm/environment.sh
+++ .oh-my-shell/shellrc/plugins/nvm/environment.sh
@@ -2,10 +2,30 @@
 mkdir -p ${HOME}/.nvm > /dev/null 2>&1
 export NVM_DIR="${HOME}/.nvm"
 
-# Load nvm shell functions
-# - ignoring desactivated nvm error due to:
-#  - nvm is not compatible with the npm config "prefix" option: currently set to "${HOME}/.npm"
-#  - Run `npm config delete prefix` or `nvm use --delete-prefix v8.11.4 --silent` to unset it.
-HOMEBREW_NVM_SCRIPT=$(homebrew_package_path_prefix "/nvm/nvm.sh")
-dot_if_exists "$HOMEBREW_NVM_SCRIPT" 
+function loadNVMSlow() {
+    # Load nvm shell functions the official way but slow
+    # - ignoring desactivated nvm error due to:
+    #  - nvm is not compatible with the npm config "prefix" option: currently set to "${HOME}/.npm"
+    #  - Run `npm config delete prefix` or `nvm use --delete-prefix v8.11.4 --silent` to unset it.
+    HOMEBREW_NVM_SCRIPT=$(homebrew_package_path_prefix "/nvm/nvm.sh")
+    dot_if_exists "$HOMEBREW_NVM_SCRIPT" 
+}
+
+function loadNVMFast() {
+    # Load nvm shell functions fast. 
+    # src: https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html
+    # - Assumes:
+    #   -- NVM was installed from Homebrew
+    #   -- nvm `default` alias set. 
+    # - Why ? 
+    #   -- Avoids doing slow sanity checks.
+    # - Caveats:
+    #   -- No check means NVM may end up failing or not working without a clear error message
+    HOMEBREW_NVM_SCRIPT=$(homebrew_package_path_prefix "/nvm/nvm.sh")
+    source "$HOMEBREW_NVM_SCRIPT" --no-use
+    NODE_VERSION=$(cat $NVM_DIR/alias/default)
+    export PATH="$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH"
+}
+
+loadNVMFast &> /dev/null
 
--- .oh-my-shell/shellrc/plugins/nvm/post.sh
+++ .oh-my-shell/shellrc/plugins/nvm/post.sh
@@ -1,2 +1,17 @@
 # Use default nvm managed node version
-nvm use --delete-prefix default --silent
+
+function nvmInstallDefaultNode() {
+    nvm install --lts &> /dev/null
+    nvm alias default node &> /dev/null
+}
+
+function nvmSetDefaultNode() {
+    nvm use --delete-prefix default --silent
+}
+
+NVM_PLUGIN_DEFAULT_NODE_INSTALLED="$HOME/.nvm/default.node.installed"
+
+[ ! -f "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED" ] && nvmInstallDefaultNode && "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED"
+# Slow loading
+[ -f "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED" ] && nvmSetDefaultNode
+
