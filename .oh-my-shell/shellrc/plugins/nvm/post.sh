32d7a5922a9d3a727ced5f44974586495d9aea30
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-06-28 19:56:18 +0800

    nvm: Optimised selecting the default node version

--- .oh-my-shell/shellrc/plugins/nvm/post.sh
+++ .oh-my-shell/shellrc/plugins/nvm/post.sh
@@ -9,9 +9,10 @@ function nvmSetDefaultNode() {
     nvm use --delete-prefix default --silent
 }
 
-NVM_PLUGIN_DEFAULT_NODE_INSTALLED="$HOME/.nvm/default.node.installed"
+USER_NVM_DIR_PATH="$HOME/.nvm"
+NVM_PLUGIN_DEFAULT_NODE_INSTALLED="$USER_NVM_DIR_PATH/default.node.installed"
 
-[ ! -f "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED" ] && nvmInstallDefaultNode && "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED"
+[ ! -f "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED" ] && nvmInstallDefaultNode && mkdir -p "$USER_NVM_DIR_PATH" && touch "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED"
 # Slow loading
 [ -f "$NVM_PLUGIN_DEFAULT_NODE_INSTALLED" ] && nvmSetDefaultNode
 
