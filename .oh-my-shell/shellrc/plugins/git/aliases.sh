059a18a04f1483eb6818d80e68023738f8db0a46
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-08-05 15:52:59 +0800

    shell: Added alias "pullUpdatesForAllGitRepositoriesAtPath"

---
+++ .oh-my-shell/shellrc/plugins/git/aliases.sh
@@ -0,0 +1,4 @@
+# Usage: Perform "git pull" on all local reposities ending with '.git' _directly_ under the specificed path
+# usage: pullUpdatesForAllGitRepositoriesAtPath "/PATH/TO/DIR/"
+alias pullUpdatesForAllGitRepositoriesAtPath='f_git_pullUpdatesForAllRepositoriesAtPath '
+ 
--- .oh-my-shell/shellrc/plugins/git/functions.sh
+++ .oh-my-shell/shellrc/plugins/git/functions.sh
@@ -1,2 +1,9 @@
 # Look into this https://github.com/junegunn/fzf/wiki/examples#git
 
+# Perform "git pull" on all local reposities ending with '.git' _directly_ under the specificed path
+# usage: f_git_pullUpdatesForAllRepositoriesAtPath "/PATH/TO/DIR/"
+function f_git_pullUpdatesForAllRepositoriesAtPath() {
+    # src: https://stackoverflow.com/q/3497123/219728
+    local dirPath="$1"
+    find $dirPath -type d -name '*.git' -maxdepth 1 -mindepth 1 | xargs -n1 -I % bash -x -c 'cd %; cd $(git rev-parse --show-toplevel) && git pull'
+}
