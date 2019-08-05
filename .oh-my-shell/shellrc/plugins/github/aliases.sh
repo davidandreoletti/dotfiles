3aa13986b71d55b9a8445845a48bd3e7db6ff1ce
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-08-05 16:11:54 +0800

    shellrc: Automated cloning all repositories from user or organization, using file as input parameters.

--- .oh-my-shell/shellrc/plugins/github/aliases.sh
+++ .oh-my-shell/shellrc/plugins/github/aliases.sh
@@ -1,2 +1,7 @@
+# Clone all repositories from a github user or organization:
 #Usage f_github_clone_all_repos "users OR orgs" "davidandreoletti OR EpicGames" "GITHUB_PERSONAL_ACCESS_TOKEN"
 alias githubCloneAllRepos='f_github_clone_all_repos '
+
+# Clone all repositories from a github user or organization as specified by a file
+#Usage f_github_clone_all_repos_with_file_as_input "/absolute/path/to/file"
+alias githubCloneAllReposUsingFile='f_github_clone_all_repos_with_file_as_input '
--- .oh-my-shell/shellrc/plugins/github/functions.sh
+++ .oh-my-shell/shellrc/plugins/github/functions.sh
@@ -5,5 +5,16 @@ function f_github_clone_all_repos () {
     local NAME="$2"; 
     local GITHUB_API_TOKEN="$3";
     PAGE=1
-    curl "https://api.github.com/$CNTX/$NAME/repos?access_token=$GITHUB_API_TOKEN&per_page=1000" | grep -o 'git@[^"]*' | xargs -I _ -P4 -L1 git clone
+    curl "https://api.github.com/$CNTX/$NAME/repos?access_token=$GITHUB_API_TOKEN&per_page=1000" | grep -o 'git@[^"]*' | xargs -I _ -P4 -L1 git clone _
+}
+
+# Usage: f_github_clone_all_repos_with_file_as_input "/absolute/path/to/file"
+# FILE FORMAT:
+# CNTX="Type of listing requested: orgs OR users"
+# NAME="Organization name or user name"
+# GITHUB_API_TOKEN="Github Access API token to use to query the organization or user's repositories listing"
+function f_github_clone_all_repos_with_file_as_input () {
+    local pathToFile="$1"
+    source $pathToFile
+    f_github_clone_all_repos "$CNTX" "$NAME" "$GITHUB_API_TOKEN"
 }
