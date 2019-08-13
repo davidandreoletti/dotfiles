93284883130d0db6b9f81386648e3d67feecd75b
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-08-13 13:07:16 +0800

    homebrew: brew linksapps is deprecated. Revert to install GUI apps to /Applications by default.

--- .oh-my-shell/shellrc/plugins/homebrew/environment.sh
+++ .oh-my-shell/shellrc/plugins/homebrew/environment.sh
@@ -1,6 +1,6 @@
 # Homebrew cask default option
 # --appdir Location where cask apps are installed
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # Do not share analytics data
 export HOMEBREW_NO_ANALYTICS=1
