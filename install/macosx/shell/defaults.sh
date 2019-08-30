f27dab5c0f7d905da94cbe06493a05ba45fe2ec9
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-08-30 20:00:01 +0800

    bootstrap: Removed explicit approval of chrome extension installation from github and al on macOS

--- install/macosx/shell/defaults.sh
+++ install/macosx/shell/defaults.sh
@@ -455,9 +455,6 @@ defaults write com.apple.CrashReporter UseUNC 1
 ###############################################################################
 # Google Chrome & Google Chrome Canary #
 ###############################################################################
-# Allow installing user scripts via GitHub Gist or Userscripts.org
-defaults write com.google.Chrome ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
-defaults write com.google.Chrome.canary ExtensionInstallSources -array "https://gist.githubusercontent.com/" "http://userscripts.org/*"
 # Disable the all too sensitive backswipe
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
