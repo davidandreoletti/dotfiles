fcccba77c6c0191e0dc6038ffd218182e182181d
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-09-05 21:54:44 +0800

    shellrc: Added concatImagesHorizontally alias

--- .oh-my-shell/shellrc/plugins/image/aliases.sh
+++ .oh-my-shell/shellrc/plugins/image/aliases.sh
@@ -5,3 +5,5 @@
 # Usage: concatImagesVertically in1.jpg in2.jpg in3.jpg out.jpg
 alias concatImagesVertically="convert -append "
 
+# Usage: concatImagesHorizontally in1.jpg in2.jpg in3.jpg out.jpg
+alias concatImagesVertically="convert +append "
