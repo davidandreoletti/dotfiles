13a800cb7400f1358ae904a3ccf645380bb7a6a6
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-09-09 21:18:43 +0800

    shellrc: Fixed loading completion for homebrew installed tools on bash

--- .oh-my-shell/bash/completion/default.sh
+++ .oh-my-shell/bash/completion/default.sh
@@ -2,8 +2,14 @@
 # Note: List all completions routine with "complete -p"
 
 # Completion for installed homebrew packages, with completion profile.d support  
-HOMEBREW_PROFILED_COMPLETION_DIR=$(homebrew_package_path_prefix "/etc/profile.d/bash_completion.sh")
-dot_if_exists "$HOMEBREW_PROFILED_COMPLETION_DIR"
+HOMEBREW_PROFILED_COMPLETION_FILE=$(homebrew_package_path_prefix "/../etc/profile.d/bash_completion.sh")
+if [[ -r "$HOMEBREW_PROFILED_COMPLETION_FILE" ]]; then
+  dot_if_exists "$HOMEBREW_PROFILED_COMPLETION_FILE"
+else
+  for COMPLETION in "$homebrew_package_path_prefix/../etc/bash_completion.d/"*; do
+    dot_if_exists "$COMPLETION"
+  done
+fi
 
 # Completion for installed homebrew packages, without completion profile.d support  
 HOMEBREW_FZF_COMPLETION_DIR=$(homebrew_package_path_prefix "/fzf/shell/completion.bash")
