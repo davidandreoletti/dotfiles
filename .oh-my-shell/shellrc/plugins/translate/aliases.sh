da466583907494aef97b5512c30446ed18cfa7ea
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-09-17 13:29:11 +0800

    shellrc: Added trsfr and trsen to translate text into french and english respectively

--- .oh-my-shell/shellrc/plugins/translate/aliases.sh
+++ .oh-my-shell/shellrc/plugins/translate/aliases.sh
@@ -1,1 +1,7 @@
+# Translate text into any language
+# Usage: trs :fr Hello
 alias trs='trans '
+# Translate text into french
+alias trsfr='trans :fr'
+# Translate text into english
+alias trseng='trans :en-GB'
