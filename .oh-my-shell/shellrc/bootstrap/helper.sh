1a682b6ceba32d078396a87b8047f561f52519fb
David Andreoletti <david.andreoletti.dev@gmail.com>
2019-10-05 23:34:12 +0800

    shellrc: Load plugins "delayed steps" such as "completions" and "post", after all plugins have loaded and shell loading its own built-in plugins such as "completion"

--- .oh-my-shell/shellrc/bootstrap/helper.sh
+++ .oh-my-shell/shellrc/bootstrap/helper.sh
@@ -43,6 +43,38 @@ if_dir_exists() {
     return
 }
 
+function shell_session_step_file () {
+    local stepName="${1:-"none"}"
+    local filePath="/tmp/${USER}_${SHELL_SESSION_ID}_$stepName"
+    echo "$filePath"
+}
+
+# Usage: dot_delayed_plugins_step_if_exists STEP NAME
+dot_delayed_plugins_step_if_exists() {
+    local stepName="${1:-"none"}"
+
+    #set -x
+    #startTime=$(command date "+%s%3N")
+    #startTime="${startTime//[!0-9]/}"
+    #set +x
+
+    local stepFile=$(shell_session_step_file "$stepName")
+    dot_if_exists "$stepFile"
+    rm -f "$stepFile"
+
+    #set -x
+    #endTime=$(command date "+%s%3N")
+    #endTime="${endTime//[!0-9]/}"
+    #runtime=$(echo "$endTime - $startTime" | bc)
+    #set +x
+    # Report plugins loading slower than 200 milliseconds
+    #if [[ $runtime -gt 200 ]];
+    #then
+    #    #echo "$startTime > $endTime"
+    #    echo "$runtime ms -> $pluginName"
+    #fi
+}
+
 # Usage: dot_plugin_if_exists PLUGIN_NAME
 dot_plugin_if_exists() {
     local pluginName="${1:-"none"}"
@@ -57,9 +89,12 @@ dot_plugin_if_exists() {
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/environment.sh"
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/functions.sh"
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/aliases.sh"
-    dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/completions.sh"
-    # FIXME: post.sh must be executed when all plugins have been initialized
-    dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/post.sh"
+
+    local stepCompletionFile=$(shell_session_step_file "completions")
+    echo "dot_if_exists ${SHELLRC_CURRENT_PLUGIN_DIR}/completions.sh" >> "$stepCompletionFile"
+
+    local stepPostFile=$(shell_session_step_file "post")
+    echo "dot_if_exists ${SHELLRC_CURRENT_PLUGIN_DIR}/post.sh" >> "$stepPostFile"
     unset SHELLRC_CURRENT_PLUGIN_DIR 
 
     #set -x
