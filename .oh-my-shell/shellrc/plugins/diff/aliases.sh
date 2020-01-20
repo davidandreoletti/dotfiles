b3acbcb8ce4032743915c82d58921a9d471eeb97
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-01-20 11:42:34 +0800

    shellrc: diff alias show diffs, using full terminal window width and wrapping line if larger than the terminal

--- .oh-my-shell/shellrc/plugins/diff/aliases.sh
+++ .oh-my-shell/shellrc/plugins/diff/aliases.sh
@@ -1,1 +1,11 @@
-alias diff="diff --side-by-side"
+# Diff, files side by side, using as much of the terminal view as possible. text will be wrapped if larger than the diff's window
+function f_diff {
+    local f1="$1"
+    local f2="$2"
+    local TERMINAL_VIEW_WIDTH=$(( $(tput cols) - 2 )); 
+    local SIDE_VIEW_WIDTH=$(echo "scale=0 ; $TERMINAL_VIEW_WIDTH / 2" | bc);
+    diff -y -W $TERMINAL_VIEW_WIDTH <( cat $f1 | fold -w $SIDE_VIEW_WIDTH ) <( cat $f2 | fold -w $SIDE_VIEW_WIDTH )
+}
+
+alias diff='f_diff '
+
