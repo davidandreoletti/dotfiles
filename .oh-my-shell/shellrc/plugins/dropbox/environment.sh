dbd766a9838749429ee5efc651e8cca061827501
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-03-18 21:38:39 +0800

    shellrc: Exporting dropbox env variable only if dropbox info file exists

--- .oh-my-shell/shellrc/plugins/dropbox/environment.sh
+++ .oh-my-shell/shellrc/plugins/dropbox/environment.sh
@@ -1,6 +1,8 @@
 # src: https://help.dropbox.com/installs-integrations/desktop/locate-dropbox-folder#programmatically
 DROPBOX_INFO_FILE="$HOME/.dropbox/info.json"
 
-export DROPBOX_PERSONAL_DIR="$(jq -r ".personal.path" $DROPBOX_INFO_FILE)"
-export DROPBOX_WORK_DIR="$(jq -r ".business.path" $DROPBOX_INFO_FILE)"
-
+if [ -f "$DROPBOX_INFO_FILE" ]
+then
+    export DROPBOX_PERSONAL_DIR="$(jq -r ".personal.path" $DROPBOX_INFO_FILE)"
+    export DROPBOX_WORK_DIR="$(jq -r ".business.path" $DROPBOX_INFO_FILE)"
+fi
