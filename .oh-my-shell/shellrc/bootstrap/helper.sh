6ba182f7e9cfa9d09de472c28b88ef606b92ccbd
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-05-07 12:46:28 +0800

    misc: Renaming various function / defaults

--- .oh-my-shell/shellrc/bootstrap/helper.sh
+++ .oh-my-shell/shellrc/bootstrap/helper.sh
@@ -135,7 +135,7 @@ function dot_plugin_if_exists() {
 }
 
 # Usage: dot_current_shell_plugin_if_exists PLUGIN_NAME
-dot_current_shell_plugin_if_exists() {
+function dot_current_shell_plugin_if_exists() {
     local pluginName="${1:-"none"}"
     export SHELLRC_CURRENT_PLUGIN_DIR="${SHELLRC_CURRENT_SHELL_DIR}/${pluginName}"
     dot_if_exists "${SHELLRC_CURRENT_PLUGIN_DIR}/default.sh"
