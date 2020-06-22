2372c97a656b3d1c9ae26e6d1f9ba776f2ef08d7
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-06-22 18:02:10 +0800

    shellrc: Fixed alias to access notebook

--- .oh-my-shell/shellrc/plugins/notebook/aliases.sh
+++ .oh-my-shell/shellrc/plugins/notebook/aliases.sh
@@ -1,1 +1,1 @@
-alias notebook="command cd $NOTEBOOK_HOME 2> /dev/null || echo \"No notebook found at $NOTEBOOK_HOME\""
+alias notebook="cd $NOTEBOOK_HOME 2> /dev/null || echo \"No notebook found at $NOTEBOOK_HOME\""
