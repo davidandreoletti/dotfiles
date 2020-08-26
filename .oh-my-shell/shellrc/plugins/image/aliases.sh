43e5bed9ae2ab960caa3ad484f7f8e84cbdfd22c
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-08-26 22:45:29 +0800

    shellrc: Fix image concatenation alias name

--- .oh-my-shell/shellrc/plugins/image/aliases.sh
+++ .oh-my-shell/shellrc/plugins/image/aliases.sh
@@ -6,4 +6,4 @@
 alias concatImagesVertically="convert -append "
 
 # Usage: concatImagesHorizontally in1.jpg in2.jpg in3.jpg out.jpg
-alias concatImagesVertically="convert +append "
+alias concatImagesHorizontally="convert +append "
