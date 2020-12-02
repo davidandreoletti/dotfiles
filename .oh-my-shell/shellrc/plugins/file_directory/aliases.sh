70e6a6890f2c9ef66cdf01de47b906e6fd739c32
David Andreoletti <david.andreoletti.dev@gmail.com>
2020-12-02 11:42:43 +0800

    shellrc: Added alias to sort directory by size

--- .oh-my-shell/shellrc/plugins/file_directory/aliases.sh
+++ .oh-my-shell/shellrc/plugins/file_directory/aliases.sh
@@ -1,3 +1,9 @@
 # Swap 2 files/or directories
 # Usage: swap file1 file2
 alias swap='f_file_directory_swap '
+
+# Find largest directory, sorted by largest to smaller
+# https://serverfault.com/a/156648
+# regquires gnu coreutils
+alias largest_dirs_asc='du -hs * | sort -h'
+
