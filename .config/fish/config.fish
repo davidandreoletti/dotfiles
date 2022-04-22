91c2c18628b66c421f7e1273ee70a89e0139f6fc
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-04-23 03:14:53 +0800

    fish: default config

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
