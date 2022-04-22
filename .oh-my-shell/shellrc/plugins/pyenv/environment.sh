b544245bc7ac5ed325e0a98672406a190ac6e848
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-04-23 03:11:56 +0800

    pyenv: Doc not wanting to hide the virtualenv prompt when env is activated.

--- .oh-my-shell/shellrc/plugins/pyenv/environment.sh
+++ .oh-my-shell/shellrc/plugins/pyenv/environment.sh
@@ -2,6 +2,10 @@
 # - https://opensource.com/article/19/5/python-3-default-mac
 export PYENV_ROOT="$HOME/.pyenv"
 
+# src: https://github.com/pyenv/pyenv-virtualenv/issues/135
+# Not sure I want this yet
+#export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+
 # Init pyenv:
 # - Define where pyenv repo is cloned 
 # - Path to pyenv binary
