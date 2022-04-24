b6894a81af3a87ae18caafb9b5676a751000684a
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-04-25 01:20:04 +0800

    boostrap: Fixed homebrew fix write dir for current user

--- install/bootstrap_macosx.sh
+++ install/bootstrap_macosx.sh
@@ -86,7 +86,7 @@ read
 message_info_show "Homebrew install ..."
 homebrew_is_installed || homebrew_install  #FIXME ask for password 
 homebrew_is_installed || message_error_show "failed"
-homebrew_is_installed && is_profile_admin_or_similar && homebrew_fix_writable_dirs "$whoami"
+homebrew_is_installed && is_profile_admin_or_similar && homebrew_fix_writable_dirs "$(whoami)"
 homebrew_is_installed || exit 1
 
 ## Command line applications
