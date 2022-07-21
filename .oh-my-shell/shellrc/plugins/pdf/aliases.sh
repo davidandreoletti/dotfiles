7f684e0f35881661d8c1b5574faa48fc045a8efa
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-07-21 23:41:38 +0800

    shellrc: Added pdf plugin

---
+++ .oh-my-shell/shellrc/plugins/pdf/aliases.sh
@@ -0,0 +1,10 @@
+# Reduce PDF file to screen quality
+# Usage: pdf_reduce_size_to_screen_quality source.pdf destination.pdf
+function pdf_reduce_size_to_screen_quality {
+    local input_file="$1"
+    local output_file="$2"
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 \
+        -dPDFSETTINGS=/screen -dNOPAUSE  \
+        -dQUIET -dBATCH  \
+        -sOutputFile="$output_file" "$input_file"
+}
