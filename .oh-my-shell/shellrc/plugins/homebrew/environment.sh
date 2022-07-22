9f79e9288fb6916baf56f660ec6b98e848152197
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-07-22 23:18:53 +0800

    shellrc: Homebrew Apple Silicon preparation

--- .oh-my-shell/shellrc/plugins/homebrew/environment.sh
+++ .oh-my-shell/shellrc/plugins/homebrew/environment.sh
@@ -5,3 +5,10 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 # Do not share analytics data
 export HOMEBREW_NO_ANALYTICS=1
 
+# Init homebrew
+# TODO: Load native homebrew at runtime using 'uname -v' varying output per cpu: "RELEASE_X86_64" (x86) vs "RELEASE_ARM64_xxxx" (ARM)
+# - x86 arch
+eval "$(/usr/local/Homebrew/bin/brew shellenv)"
+# - ARM arch
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
