a64c9bf5a45c5169507636b8c769bdf70756886e
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-08-02 00:31:30 +0800

    shellrc: Added alias xcodePurgeAllCaches

--- .oh-my-shell/shellrc/plugins/xcode/aliases.sh
+++ .oh-my-shell/shellrc/plugins/xcode/aliases.sh
@@ -1,3 +1,16 @@
 # http://stackoverflow.com/a/18933476/219728
 alias xcodePurgeDerivedData='rm -rf ~/library/Developer/Xcode/DerivedData/*'
 
+alias xcodePurgeAllCaches='(
+echo "Delete Archived Applications"
+rm -rfv ~/Library/Developer/Xcode/Archives/*/
+echo "Delete Devired Data"
+rm -rfv ~/Library/Developer/Xcode/DerivedData/*/
+echo "Delete Apple cached files"
+rm -rfv ~/Library/Developer/CoreSimulator/Caches/dyld/*/*/
+echo "Delegete cache on com.apple.DeveloperTools"
+rm -rfv /private/var/folders/dk/*/C/com.apple.DeveloperTools/*/
+echo "Delete unused simulators"
+xcrun simctl delete unavailable
+)'
+
