a8c8b33707c21aa7f7b10940d5fff6c72777d07f
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-08-02 00:30:36 +0800

    shellrc: Added alias archiveFolder

--- .oh-my-shell/shellrc/plugins/archive/aliases.sh
+++ .oh-my-shell/shellrc/plugins/archive/aliases.sh
@@ -4,3 +4,8 @@ alias uco='unp'
 # Archive as zip file each root level folder in the specified path
 # Usage: archiveEachFolderSeparatidely "/path/to/a/folder"
 alias archiveEachFolderSeparatidely='f_archiveEachFolderSeparatedely'
+
+# Archive as tar.gz the specified folder
+# Usage: archiveFolder /path/to/my/folder
+# - Note: archive name is archive.tar.gz
+alias archiveFolder='tar -czvf archive.tar.gz '
