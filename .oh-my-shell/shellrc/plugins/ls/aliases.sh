ec82f4e5cb8af5c7ebcf23f11f8fa474a3fb207d
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-09-23 21:42:34 +0800

    shellrc: Cleanup ls aliases

--- .oh-my-shell/shellrc/plugins/ls/aliases.sh
+++ .oh-my-shell/shellrc/plugins/ls/aliases.sh
@@ -1,14 +1,13 @@
 # Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
+if ls --color > /dev/null 2>&1; 
+then # GNU `ls`
     colorflag="--color"
 else # OS X `ls`
     colorflag="-G"
 fi
 
 # List all files colorized in long format, including dot files, including file allocation and file max size
-alias l="ls -slahF ${colorflag} --time-style=long-iso "
+alias l="command ls -slahF ${colorflag} --time-style=long-iso "
 # List only directories
-alias lsd="ls -lhF ${colorflag} --time-style=long-iso | grep --color=never '^d'"
-# List files colorized
-alias ls="command ls ${colorflag}"
+alias lsd="command ls -lhF ${colorflag} --time-style=long-iso | grep --color=never '^d' "
 
