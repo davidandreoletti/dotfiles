d829c4e1027b41b65225ece7a7f1876c2107e4a0
David Andreoletti <david.andreoletti.dev@gmail.com>
2022-10-28 00:25:40 +0800

    shellrc: Disable asdf java plugin for now.

--- .oh-my-shell/shellrc/plugins/java/post.sh
+++ .oh-my-shell/shellrc/plugins/java/post.sh
@@ -6,10 +6,9 @@ if [[ "$OS_NAME" == "macosx" ]]; then
     #eval "$(jenv init - --no-rehash)"
     (
         # Register Installed Java SDKs with jenv
-        find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\" > /dev/null 2>&1" \;
-        find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -xtype d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\" > /dev/null 2>&1" \;
-        jenv rehash
-    ) 2> /dev/null 
+        find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\" > /dev/null 2>&1" \; &
+        jenv rehash &
+    ) 2> /dev/null
 
     # jenv used to select a specific java version
     #jenv enable-plugin export > /dev/null  2>&1; 
@@ -19,16 +18,21 @@ if [[ "$OS_NAME" == "macosx" ]]; then
     #echo "$mostRecentJavaVersion" > $HOME/.java-version
 
 
-    ########################################################
+    #
     # IMPORTANT: jenv's initialization unsets JAVA_HOME.
     # User is expected to set it via jenv
-    ########################################################
     #
-	# 1. Export JAVA_HOME for GUI/shell applications
+
+	# Export JAVA_HOME for GUI/shell applications
+	#
+
 	# Java version used: most recent installed
     launchctl setenv JAVA_HOME `/usr/libexec/java_home`
 
-    # 2. Export "JAVA_HOME" for shell applications
-    # Java version used: 1.8.x for tooling like android/gradle
+    #
+    # Export "JAVA_HOME" for shell applications
+    #
+
+    # Javaversion used: 1.8.x) for tooling like android/gradle
     export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
 fi
