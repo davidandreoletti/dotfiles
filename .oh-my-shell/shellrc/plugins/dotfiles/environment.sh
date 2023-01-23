4a391cfd384b3ef4086fe5bec68dcbee33cd5d02
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-01-24 00:55:35 +0800

    dotfiles: Resolved dotfiles-private dir when sourced symlink points to a file instead of dir

--- .oh-my-shell/shellrc/plugins/dotfiles/environment.sh
+++ .oh-my-shell/shellrc/plugins/dotfiles/environment.sh
@@ -1,2 +1,2 @@
 export DOTFILES_HOME=$(readlink -f "$HOME/.oh-my-shell/../")
-export DOTFILES_PRIVATE_HOME="$(readlink -f $HOME/.is-encrypted/../)"
+export DOTFILES_PRIVATE_HOME="$(dirname $(readlink -f $HOME/.is-encrypted))"
