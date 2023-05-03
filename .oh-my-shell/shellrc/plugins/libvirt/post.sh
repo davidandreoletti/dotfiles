be97108cccdff193494b8bdd777ad09546f9ccf7
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-05-03 11:45:16 +0800

    shellrc: Use valid fd number on bash / zsh

--- .oh-my-shell/shellrc/plugins/libvirt/post.sh
+++ .oh-my-shell/shellrc/plugins/libvirt/post.sh
@@ -1,5 +1,5 @@
 # Start libvirt daemon for virt-manager to connect to it, if installed
 (
-    flock --nonblock 100 || exit 0
+    flock --nonblock 9 || exit 0
     brew services list | grep "libvirt" && brew services start libvirt & 
-) 100> /tmp/shellrc.libvirt.lock > /dev/null & 
+) 9>/tmp/shellrc.libvirt.lock > /dev/null & 
