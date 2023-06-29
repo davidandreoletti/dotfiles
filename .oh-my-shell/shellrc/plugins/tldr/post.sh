0b19a64eb31a56e19169d977d0b3a8bda05d23fe
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-06-29 21:00:41 +0800

    shellrc: Update tldr database every 7d

---
+++ .oh-my-shell/shellrc/plugins/tldr/post.sh
@@ -0,0 +1,17 @@
+TLDR_POST_BASE="/tmp/${USER}_tldr_post"
+TLDR_POST_MARKER="${TLDR_POST_BASE}.marker"
+
+# 3600*24*7 = 7 days
+if f_run_every_x_seconds "$TLDR_POST_MARKER" "$((3600*24*7))" ;
+then
+
+    # Always update the local database
+    TLDR_INSTALL_SCRIPT="${TLDR_POST_BASE}.install.sh"
+    TLDR_INSTALL_LOG="${TLDR_INSTALL_SCRIPT}.log"
+
+cat <<EOF > "$TLDR_INSTALL_SCRIPT"
+    tldr --update
+EOF
+
+    f_run_exclusive_in_background_with_completion "${TLDR_INSTALL_SCRIPT}.lockfile" "$TLDR_INSTALL_LOG" "$TLDR_POST_MARKER" bash $TLDR_INSTALL_SCRIPT
+fi
