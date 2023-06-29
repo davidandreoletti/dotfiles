d492ddf0563a78f5ee591354140d0494d3053ccc
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-06-29 21:10:10 +0800

    shellrc: Added oman alias

--- .oh-my-shell/shellrc/plugins/man/aliases.sh
+++ .oh-my-shell/shellrc/plugins/man/aliases.sh
@@ -1,3 +1,7 @@
 # Use tldr when CLI_APP_NAME is documented. Otherwise, fallback to standard man
 # Usage: man CLI_APP_NAME
 alias man='f_man_prefer_tldr_when_available '
+
+# Always use default man page for CLI_APP_NAME 
+# Usage: oman CLI_APP_NAME
+alias oman='command man '
