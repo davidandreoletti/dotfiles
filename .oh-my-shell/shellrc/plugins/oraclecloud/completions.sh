03e0bf7557d30b522ee9538804062fdd1f38039e
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-07-04 10:44:51 +0800

    shellrc: Load oci shell completions when oci installed

--- .oh-my-shell/shellrc/plugins/oraclecloud/completions.sh
+++ .oh-my-shell/shellrc/plugins/oraclecloud/completions.sh
@@ -1,1 +1,5 @@
-eval "$(_OCI_COMPLETE=source oci)"
+# oci may not be installed on all machines, conditionally load oci shell completions
+if command -v oci > /dev/null ];
+then
+    eval "$(_OCI_COMPLETE=source oci)"
+fi
