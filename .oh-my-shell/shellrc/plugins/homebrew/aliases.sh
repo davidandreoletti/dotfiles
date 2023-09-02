5d0dfacf18b4d3fa68931a752fcad92050ca6cd9
David Andreoletti <david@andreoletti.net>
2023-09-03 07:51:08 +0800

    shellrc: Added alias to list homebrew's installed keg only

--- .oh-my-shell/shellrc/plugins/homebrew/aliases.sh
+++ .oh-my-shell/shellrc/plugins/homebrew/aliases.sh
@@ -1,3 +1,5 @@
 # Update already installed non cask and cask brew packages
 alias brewUpgrades="brew update; brew cleanup -s; brew upgrade; brew cask upgrade --greedy --force; brew prune; brew doctor; brew missing"
+# List installed homebrew keg-only
+alias brewListInstalledKegOnly="brew info --installed --json=v1 | jq 'map(select(.keg_only == true)) | map(.name)'"
 
