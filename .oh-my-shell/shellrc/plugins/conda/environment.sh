7d62d4e759933215c14231482d39ce1349bc00ce
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-02 12:19:54 +0800

    shellrc: Added shell name to conda init

--- .oh-my-shell/shellrc/plugins/conda/environment.sh
+++ .oh-my-shell/shellrc/plugins/conda/environment.sh
@@ -2,10 +2,10 @@
 # - init code completion, etc
 if [[ "$OS_NAME" == "macosx" ]];
 then
-    [ -d "$HOME/.conda" ] || ( conda init --quiet )
+    [ -d "$HOME/.conda" ] || ( conda init --quiet $SHELL_NAME )
 else
     # Linux does not seem to need it per shell instance
-    # However sudo conda initi --quiet ran during bootstrap_apps.sh
+    # However sudo conda init --quiet ran during bootstrap_apps.sh
     :
 fi
 
