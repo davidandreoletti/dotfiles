67d9f42872a66c34efc0f2ef78657319c7056ede
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-05 15:59:36 +0800

    shellrc: Disable brew services start libvirt

--- .oh-my-shell/shellrc/plugins/libvirt/post.sh
+++ .oh-my-shell/shellrc/plugins/libvirt/post.sh
@@ -1,5 +1,9 @@
+#
+# Disable starting livirt service on macOS since virt-manager does not work well on macOS for now
+#
+
 # Start libvirt daemon for virt-manager to connect to it, if installed
-(
-    flock --nonblock 9 || exit 0
-    brew services list | grep "libvirt" && brew services start libvirt & 
-) 9>/tmp/shellrc.libvirt.lock > /dev/null & 
+#(
+#    flock --nonblock 9 || exit 0
+#    brew services list | grep "libvirt" && brew services start libvirt & 
+#) 9>/tmp/shellrc.libvirt.lock > /dev/null & 
