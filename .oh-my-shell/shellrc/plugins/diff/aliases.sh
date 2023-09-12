e342b89953b1ec3b77fbdc2daf619f150b1bc8f8
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-12 23:28:32 +0800

    shellrc: allow optional diff args

--- .oh-my-shell/shellrc/plugins/diff/aliases.sh
+++ .oh-my-shell/shellrc/plugins/diff/aliases.sh
@@ -2,10 +2,15 @@
 function f_diff {
     local f1="$1"
     local f2="$2"
+    shift
+    shift
+    local args="$@"
     local TERMINAL_VIEW_WIDTH=$(( $(tput cols) - 2 )); 
     local SIDE_VIEW_WIDTH=$(echo "scale=0 ; $TERMINAL_VIEW_WIDTH / 2" | bc);
-    diff -y -W $TERMINAL_VIEW_WIDTH <( cat $f1 | fold -w $SIDE_VIEW_WIDTH ) <( cat $f2 | fold -w $SIDE_VIEW_WIDTH )
+    command diff $args -y -W $TERMINAL_VIEW_WIDTH <( cat $f1 | fold -w $SIDE_VIEW_WIDTH ) <( cat $f2 | fold -w $SIDE_VIEW_WIDTH )
 }
 
+# Diff 2 files
+#Usage: diff file1 file2 --suppress-common-lines
 alias diff='f_diff '
 
