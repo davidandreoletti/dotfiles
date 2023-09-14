031dc5f20927d02e4973b08846e9de91e50047f0
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-14 20:44:08 +0800

    shellrc: Disable '[x] done' from shell when executing mpd's plugin post.sh

--- .oh-my-shell/shellrc/plugins/mpd/post.sh
+++ .oh-my-shell/shellrc/plugins/mpd/post.sh
@@ -1,5 +1,7 @@
 # Start mpd daemon for any user to connect to it, if installed
 (
-    flock --nonblock 9 || exit 0
-    brew services list | grep "mpd" | grep -v "started" && brew services start mpd
-) 9>/tmp/shellrc.mpd.lock > /dev/null & 
+    (
+        flock --nonblock 9 || exit 0
+        ( brew services list | grep "mpd" | grep -v "started" && brew services start mpd ) || true
+    ) 9>/tmp/shellrc.mpd.lock > /dev/null & 
+)
