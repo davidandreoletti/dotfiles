da3cd0929118345321d1dcfeef657d8164cd6039
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-14 19:27:09 +0800

    shellrc: Added mankier alias

--- .oh-my-shell/shellrc/plugins/man/aliases.sh
+++ .oh-my-shell/shellrc/plugins/man/aliases.sh
@@ -5,3 +5,7 @@ alias man='f_man_prefer_tldr_when_available '
 # Always use default man page for CLI_APP_NAME 
 # Usage: oman CLI_APP_NAME
 alias oman='command man '
+
+# Open package on mankier.com
+# Usage: mankier ffmpeg
+alias mankier='f_man_open_with_mankier '
--- .oh-my-shell/shellrc/plugins/man/functions.sh
+++ .oh-my-shell/shellrc/plugins/man/functions.sh
@@ -7,3 +7,10 @@ function f_man_prefer_tldr_when_available() {
         command man $requested
     fi
 }
+
+function f_man_open_with_mankier() {
+    local package_name="$1"
+
+    echo "Opening with your browser: $package_name"
+    open "https://www.mankier.com/1/$package_name"
+}
