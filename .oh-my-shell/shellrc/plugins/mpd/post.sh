16ff82b2610f81149936d2680cb1f17e2a86411e
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-14 19:40:55 +0800

    shellrc: Fixed preventing mpd start when already started (by any users)

--- .oh-my-shell/shellrc/plugins/mpd/post.sh
+++ .oh-my-shell/shellrc/plugins/mpd/post.sh
@@ -1,5 +1,5 @@
 # Start mpd daemon for any user to connect to it, if installed
 (
     flock --nonblock 9 || exit 0
-    brew services list | grep "mpd" && grep -v "started" && brew services start mpd
+    brew services list | grep "mpd" | grep -v "started" && brew services start mpd
 ) 9>/tmp/shellrc.mpd.lock > /dev/null & 
