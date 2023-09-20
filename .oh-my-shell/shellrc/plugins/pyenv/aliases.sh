b6dacba9c3789ae8b31a605e5d52e68cf16cb086
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-09-20 16:11:04 +0800

    shellrc: reminder about libzip2 shared lib missing setup when compiling python

--- .oh-my-shell/shellrc/plugins/pyenv/aliases.sh
+++ .oh-my-shell/shellrc/plugins/pyenv/aliases.sh
@@ -1,2 +1,5 @@
 # Install optimized python version X with brew
+# libbzip2's shared lib might be have installed a libbz2.so.1.0. If so:
+# - On linux: ln -s /usr/lib64/libbz2.so.1.0.*  /usr/lib64/libbz2.so.1.0
+# - On macOS: not needed ?
 alias pyenv_install="env PYTHON_CONFIGURE_OPTS='--enable-optimizations --with-lto' PYTHON_CFLAGS='-march=native -mtune=native' pyenv install"
