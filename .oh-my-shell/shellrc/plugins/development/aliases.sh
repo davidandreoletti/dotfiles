635d8eed1d8579fc5e75bdce0a13316408874784
David Andreoletti <david.andreoletti.dev@gmail.com>
2023-10-03 12:23:04 +0800

    shellrc: Discard from menu all last directory names ending with .git/.git

--- .oh-my-shell/shellrc/plugins/development/aliases.sh
+++ .oh-my-shell/shellrc/plugins/development/aliases.sh
@@ -1,2 +1,2 @@
 alias code="cd ${_DEFAULT_DEVELOPMENT_DIR}"
-alias code_menu="cd \"${_DEFAULT_DEVELOPMENT_DIR}/\$(find ${_DEFAULT_DEVELOPMENT_DIR} -maxdepth 3 -type d -name '*.git' -printf '%P\n' | fzf)\" "
+alias code_menu="cd \"${_DEFAULT_DEVELOPMENT_DIR}/\$(find ${_DEFAULT_DEVELOPMENT_DIR} -maxdepth 3 -type d -name '*.git' -printf '%P\n' | grep -v '.git/.git' | fzf)\" "
