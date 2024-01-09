490ec510b436ab21510104f9282de5332fc5d4a0
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-10 07:53:16 +0800

    shellrc: Added tgit alias

--- .oh-my-shell/shellrc/plugins/git/aliases.sh
+++ .oh-my-shell/shellrc/plugins/git/aliases.sh
@@ -1,4 +1,11 @@
 # Usage: Perform "git pull" on all local reposities ending with '.git' _directly_ under the specificed path
 # usage: gitPullUpdatesForAllGitRepositoriesAtPath "/PATH/TO/DIR/"
 alias gitPullUpdatesForAllGitRepositoriesAtPath='f_git_pullUpdatesForAllRepositoriesAtPath '
- 
+
+# (As of v0.20.x) Lazygit does not read config file from ~/.config/lazygit on macOS
+# src: https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md#user-config
+alias lazygit="XDG_CONFIG_HOME=$HOME/.config lazygit"
+
+# Display git TUI
+# Usage: tgit
+alias tgit=lazygit
