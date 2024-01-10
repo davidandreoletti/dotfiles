445d23ec533c20a9e356a53feff35e6cb33e9f83
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-10 10:45:46 +0800

    shellrc: better default for terminal true color support

--- .oh-my-shell/shellrc/plugins/environment/environment.sh
+++ .oh-my-shell/shellrc/plugins/environment/environment.sh
@@ -6,13 +6,19 @@
 export EDITOR=`which vim`
 export VISUAL=$EDITOR
 
+# TERM reports the terminal type. Best to leave the terminal implementation report it
+# - Apple's Terminal: xterm-256color
+# - Alacritty: xterm-256color
+# - kitty: ?
+#
 # Set 256 capable color
 # Reported with:  find /lib/terminfo /usr/share/terminfo -name "*256*"
 # http://push.cx/2008/256-color-xterms-in-ubuntu
-export TERM=xterm-256color
+#export TERM=xterm-256color
 
 # Set PATH so it includes user's private bin if it exists
 [ -d "$HOME/bin" ] && path_prepend "$HOME/bin"
 
 # Load shell specific config
 dot_if_exists "${SHELLRC_CURRENT_SHELL_DIR}/environment.sh"
+
