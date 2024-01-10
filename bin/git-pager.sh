3da87f871f79107c8a0facb18bb081cc0f0d8fab
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-10 19:12:54 +0800

    git: switched pager for various commands to delta

--- bin/git-pager.sh
+++ bin/git-pager.sh
@@ -1,6 +1,7 @@
 # Set pager to display raw control characters
 
 pagerType=${1:-"simple"}
+shift
 
 case "$pagerType" in
     "simple")
@@ -8,11 +9,20 @@ case "$pagerType" in
         ;;
     "diff")
         # FIXME: Use HOMEBREW_PACKAGES_xxxx_DIR variable set in .oh-my-shell/functions.sh
-        # FIXME: Will break on debian based OSes
         GIT_DIFF_HIGHLIGHT="$(brew --prefix git)/share/git-core/contrib/diff-highlight/diff-highlight"
         if [ -r "$GIT_DIFF_HIGHLIGHT" ] 
         then
             $GIT_DIFF_HIGHLIGHT | less
+        else
+            less -r
+        fi
+        ;;
+    "delta")
+        # FIXME: Use HOMEBREW_PACKAGES_xxxx_DIR variable set in .oh-my-shell/functions.sh
+        GIT_DELTA="$(brew --prefix git-delta)/bin/delta"
+        if [ -r "$GIT_DELTA" ]
+        then
+            $GIT_DELTA "$@" | less
         else
             less -r
         fi
