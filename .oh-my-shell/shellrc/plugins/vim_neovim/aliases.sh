ccfebcf589f7b25b066571cf388d1e770a26b0c7
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-10 17:22:19 +0800

    shellrc: Added alias nvimWipeShadaFile

--- .oh-my-shell/shellrc/plugins/vim_neovim/aliases.sh
+++ .oh-my-shell/shellrc/plugins/vim_neovim/aliases.sh
@@ -3,3 +3,7 @@
 # Vim editor without plugins loaded
 # Usage: vimb /path/to/file
 alias vimn='vim -u NONE '
+
+# Wipe neovim shada file
+# src: https://neovim.io/doc/user/starting.html#shada
+alias nvimWipeShadaFile="rm -fv $HOME/.local/state/nvim/shada/main.shada"
