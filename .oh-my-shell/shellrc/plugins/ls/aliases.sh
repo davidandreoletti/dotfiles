e9697717cbec2b2b7249a60879a98725b3cbf01a
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-11 15:43:08 +0800

    ls: refactored ls aliases

--- .oh-my-shell/shellrc/plugins/ls/aliases.sh
+++ .oh-my-shell/shellrc/plugins/ls/aliases.sh
@@ -6,8 +6,22 @@ else # OS X `ls`
     colorflag="-G"
 fi
 
+listingflag="--all --human-readable -l --classify --time-style=long-iso --inode"
+
+# Original ls with colorized output
+alias ls="command ls --all --human-readable --classify ${colorflag}"
 # List all files colorized in long format, including dot files, including file allocation and file max size
-alias l="command ls -slahF ${colorflag} --time-style=long-iso "
+alias l="command ls ${listingflag} ${colorflag}"
+# List only regural files
+alias ls_file="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ -.*$' "
+# List only block special file
+alias ls_block="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ b.*$' "
+# List only character special file
+alias ls_char="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ c.*$' "
 # List only directories
-alias lsd="command ls -lhF ${colorflag} --time-style=long-iso | grep --color=never '^d' "
+alias ls_dir="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ d.*$' "
+# List only symbolic links
+alias ls_symbolic="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ l.*$' "
+# List only fifo files
+alias ls_fifo="command ls ${listingflag} ${colorflag} | grep --color=never '^\d\+ p.*$' "
 
