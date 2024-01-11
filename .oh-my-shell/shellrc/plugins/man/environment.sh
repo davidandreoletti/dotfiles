d3d099f6f6cb3870ce70272bd6db35684c0b1d67
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-12 03:14:30 +0800

    man: colorized man page

--- .oh-my-shell/shellrc/plugins/man/environment.sh
+++ .oh-my-shell/shellrc/plugins/man/environment.sh
@@ -1,2 +1,5 @@
-# Don’t clear the screen after quitting a manual page
-export MANPAGER="less -X";
+# -X: Don’t clear the screen after quitting a manual page
+# src: https://felipec.wordpress.com/2021/06/05/adventures-with-man-color/
+export MANPAGER="less -R --use-color -Dd+r -Du+b -X"
+#export MANPAGER='nvim +Man!'
+export MANROFFOPT="-c"
