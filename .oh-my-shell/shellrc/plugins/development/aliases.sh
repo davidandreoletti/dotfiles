2db1e9fcb9660b18285acb058dbbc7f455811b2e
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-11 16:31:41 +0800

    shellrc: code cleanup

--- .oh-my-shell/shellrc/plugins/development/aliases.sh
+++ .oh-my-shell/shellrc/plugins/development/aliases.sh
@@ -1,2 +1,7 @@
+# Move shell working dir to default development dir
+# usage: code
 alias code="cd ${_DEFAULT_DEVELOPMENT_DIR}"
+
+# Move shell working dir to a user select git directory within the default development dir
+# usage: tcode
 alias tcode="cd \"${_DEFAULT_DEVELOPMENT_DIR}/\$(find ${_DEFAULT_DEVELOPMENT_DIR} -maxdepth 3 -type d -name '*.git' -printf '%P\n' | grep -v '.git/.git' | fzf)\" "
