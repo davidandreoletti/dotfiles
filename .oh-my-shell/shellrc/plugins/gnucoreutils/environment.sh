ea9491ea134c79b983a0cfbc6275b2f89a266d5b
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-12 22:16:38 +0800

    shellrc: code cleanup

--- .oh-my-shell/shellrc/plugins/gnucoreutils/environment.sh
+++ .oh-my-shell/shellrc/plugins/gnucoreutils/environment.sh
@@ -2,7 +2,6 @@
 # coretutils tools (eg: dircolors) to be prefixed with g (eg: gdircolors)
 # on OSX
 # - OSX: brew install coreutils
-# FIXME: Check OS === OSX for OSX specific paths
 HOMEBREW_GNUCOREUTILS_DIR=$(homebrew_package_path_prefix "/coreutils/libexec")
 [ -d "$HOMEBREW_GNUCOREUTILS_DIR/gnubin" ] && path_prepend "$HOMEBREW_GNUCOREUTILS_DIR/gnubin"
 [ -d "$HOMEBREW_GNUCOREUTILS_DIR/gnuman" ] && manpath_prepend "$HOMEBREW_GNUCOREUTILS_DIR/gnuman"
--- .oh-my-shell/shellrc/plugins/gnused/environment.sh
+++ .oh-my-shell/shellrc/plugins/gnused/environment.sh
@@ -2,7 +2,6 @@
 # gnu-sed tools (eg: dircolors) to be prefixed with g (eg: gsed)
 # on OSX
 # - OSX: brew install gnu-sed
-# FIXME: Check OS === OSX for OSX specific paths
 HOMEBREW_GNUSED_DIR=$(homebrew_package_path_prefix "/gnu-sed/libexec")
 [ -d "$HOMEBREW_GNUSED_DIR/gnubin" ] && path_prepend "$HOMEBREW_GNUSED_DIR/gnubin"
 [ -d "$HOMEBREW_GNUSED_DIR/gnuman" ] && manpath_prepend "$HOMEBREW_GNUSED_DIR/gnuman"
