988b5b6877acec714a53c0c78d187eb4e1672ff0
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-12 21:24:17 +0800

    dotfiles: resovle dotfiles and dotfiles-private using permament file in each repo

--- .oh-my-shell/shellrc/plugins/dotfiles/environment.sh
+++ .oh-my-shell/shellrc/plugins/dotfiles/environment.sh
@@ -1,2 +1,2 @@
-export DOTFILES_HOME=$(readlink -f "$HOME/.oh-my-shell/../")
-export DOTFILES_PRIVATE_HOME="$(dirname $(readlink -f $HOME/.is-encrypted))"
+export DOTFILES_HOME=$(realpath "$(dirname $(readlink -f ~/.oh-my-shell/oh-my-shellrc))/../")
+export DOTFILES_PRIVATE_HOME="$(dirname $(readlink -f $HOME/.ssh))"
