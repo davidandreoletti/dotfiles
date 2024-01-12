22d12ef9fab88a1b1a8f6f0952d316c75e9c0ca9
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-12 12:05:20 +0800

    shellrc: added chars plugin

---
+++ .oh-my-shell/shellrc/plugins/chars/aliases.sh
@@ -0,0 +1,15 @@
+# Display a character by its value
+# usage: ascii_lookup 'ß'
+alias ascii_lookup='chars '
+
+# Display a character by its unicode point
+# usage: unicode_lookup 'U+1F63C'
+alias unicode_lookup='chars '
+
+# Display a character by char code
+# usage: char_lookup '10'
+alias char_lookup='chars '
+
+# Display a control character 
+# usage: char_lookup '^C'
+alias controlchar_lookup='chars '
