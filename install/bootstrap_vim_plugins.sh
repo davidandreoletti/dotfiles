f089bad00395848bd76d7ce2837998df503258fd
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-13 23:03:05 +0800

    bootstrap: upgrade vim-plug before installing/updating vim plugins

--- install/bootstrap_vim_plugins.sh
+++ install/bootstrap_vim_plugins.sh
@@ -2,6 +2,6 @@ for cmd in vim nvim;
 do
     if command -v $cmd >/dev/null 2>&1; then
       echo "Bootstraping $cmd ..."
-      command $cmd '+PlugUpdate' '+PlugClean!' '+PlugUpdate' '+qall'
+      command $cmd '+PlugUpgrade!' '+PlugUpdate' '+PlugClean!' '+PlugUpdate' '+qall'
     fi
 done
