5d5f2e21a3404926331a85261c8079307cd14398
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-14 01:16:49 +0800

    tmux: added alias to kill / start tmux server

--- .oh-my-shell/shellrc/plugins/tmux/aliases.sh
+++ .oh-my-shell/shellrc/plugins/tmux/aliases.sh
@@ -10,4 +10,12 @@ alias ttmux='f_tmux_open_or_create_session interactive'
 
 # Start tmux with brand new server instance and no configuration file
 # usage: tmux_none
-alias tmux_none='tmux -L test_$USER_$$ -f /dev/null'
+alias tmux_none='command tmux -L test_$USER_$$ -f /dev/null'
+
+# Clean + graceful kill all tmux session + server
+# usage: tmux_kill_server
+alias tmux_kill_server='command tmux kill-server '
+
+# Start server
+# usage: tmux_start_server
+alias tmux_start_server='command tmux start-server '
