4845dc9adfd5e746175ad7da7e6b619aaef94b0d
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-17 02:55:36 +0800

    alacritty: silence git pull/clone for themes

--- .oh-my-shell/shellrc/plugins/alacritty/post.sh
+++ .oh-my-shell/shellrc/plugins/alacritty/post.sh
@@ -1,5 +1,5 @@
 ALACRITTY_THEMES_DIR="$HOME/.config/alacritty/themes"
 
 # Clone or update themes
-git -C "$ALACRITTY_THEMES_DIR" pull > /dev/null \
-    || git clone https://github.com/alacritty/alacritty-theme "$ALACRITTY_THEMES_DIR" > /dev/null
+git -C "$ALACRITTY_THEMES_DIR" pull > /dev/null 2>&1 \
+    || git clone https://github.com/alacritty/alacritty-theme "$ALACRITTY_THEMES_DIR" > /dev/null 2>&1
