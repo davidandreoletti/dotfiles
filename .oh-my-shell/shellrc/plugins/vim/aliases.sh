7234dd2ea280c580ade2f6db7bccc806f1ba529f
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-16 17:17:37 +0800

    vim: accessible vim cheats

--- .oh-my-shell/shellrc/plugins/vim/aliases.sh
+++ .oh-my-shell/shellrc/plugins/vim/aliases.sh
@@ -1,3 +1,10 @@
-# Edit vimrc file
-# usage vim_vimrc_edit
+# % vim, edit, vimrc
+# # Edit vimrc file
+# ; usage vim_vimrc_edit
 alias vim_vimrc_edit='vim ~/.vim/vimrc'
+
+# % vim, cheatsheets
+# # Display vim cheatsheets + my own
+# ; usage: vim_cheatsheets
+# FIXME Change ',' to '<leader>: https://vi.stackexchange.com/q/43884/12436
+alias vim_cheatsheets='vim "+normal ,?"'
