9e47abfcea2078a095671af0d9c81ab9bf431f42
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-17 10:59:34 +0800

    shellrc: migrated shellrc plugin aliases documentation to to navi syntax

--- .oh-my-shell/shellrc/plugins/shellrc/aliases.sh
+++ .oh-my-shell/shellrc/plugins/shellrc/aliases.sh
@@ -1,11 +1,14 @@
-# List shellrc's plugins
-# usage: shellrc_plugin_list
+# % shellrc, plugin, list
+# # List shellrc's plugins
+# ; usage: shellrc_plugin_list
 alias shellrc_plugin_list='l $SHELLRC_PLUGINS_DIR/ | fzf'
 
-# Edit shellrc plugin function 
-# usage: shellrc_plugin_edit 'plugin_name'
+# % shellrc, plugin, edit
+# # Edit shellrc plugin function 
+# ; usage: shellrc_plugin_edit 'plugin_name'
 alias shellrc_plugin_edit='f_shellrc_plugin_edit '
 
-# Create shellrc plugin structure + function
-# usage: shellrc_plugin_create 'plugin_name'
+# % shellrc, plugin, edit
+# # Create shellrc plugin structure + function
+# ; usage: shellrc_plugin_create 'plugin_name'
 alias shellrc_plugin_create='f_shellrc_plugin_create '
