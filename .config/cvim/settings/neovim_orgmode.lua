9404853beb1902add9701ca608e212a6f05bd426
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-17 20:44:36 +0800

    nvim: nvim-orgmode initial config

--- .config/cvim/settings/neovim_orgmode.lua
+++ .config/cvim/settings/neovim_orgmode.lua
@@ -15,6 +15,23 @@ local function config()
     require('orgmode').setup({
         org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
+        -- Custom keywords for unfinished / finished states
+        org_todo_keywords = {'TODO(t)', 'WIP(w)', 'BLOCKED(b)', '|', 'DONE(d)', 'DELEGATED'},
+        -- Custom color for todo keywords
+        --org_todo_keyword_faces = {
+        --    WAITING = ':foreground blue :weight bold',
+        --    DELEGATED = ':background #FFFFFF :slant italic :underline on',
+        --    TODO = ':background #000000 :foreground red',
+        --}
+        -- Hide leading starts
+        org_hide_leading_stars = true,
+        -- Custom mapping
+        mappings = {
+            global = {
+                org_agenda = '<Leader>oa',
+                org_capture = '<Leader>oc'
+            }
+        },
     })
 end
 
