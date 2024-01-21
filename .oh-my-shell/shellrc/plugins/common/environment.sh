c3b3e0ec5816135fa3714ced52ac31138b2216d9
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-22 00:47:56 +0800

    shellrc: code cleanup

--- .oh-my-shell/shellrc/plugins/common/environment.sh
+++ .oh-my-shell/shellrc/plugins/common/environment.sh
@@ -1,11 +1,7 @@
 # Homebrew
 # Note: Use homebrew in priority (outside the homebrew plugin because some plugins called before homebrew plugin is called need 'brew' to be in the PATH)
-# case: macos
-[ -d "/usr/local/bin" ] && export PATH=/usr/local/bin:$PATH
-# case: linux
-[ -d "/home/linuxbrew/.linuxbrew/bin" ] && export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-
-export PATH
+is_macos && [ -d "/usr/local/bin" ] && path_prepend "/usr/local/bin"
+is_linux && [ -d "/home/linuxbrew/.linuxbrew/bin" ] && path_prepend "/home/linuxbrew/.linuxbrew/bin"
 
 # Check XDG directories exist
 if is_macos; then
