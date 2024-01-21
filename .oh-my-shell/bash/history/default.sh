0a37e854e4737f47e01487dcf1421a778b3399ff
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-01-21 23:38:47 +0800

    bash/zsh: increased history size / timestamp history

--- .oh-my-shell/bash/history/default.sh
+++ .oh-my-shell/bash/history/default.sh
@@ -1,17 +1,21 @@
 # Shell history
+# - History file location
+HISTFILE=$HOME/.bash_history_permanent
+# - Append command to history
 shopt -s histappend
+# - Max entries in history in RAM
+HISTSIZE=9000000
+# - Max entries in history on disk
+HISTFILESIZE=$HISTSIZE
+# History record timestamp per command
+HISTTIMEFORMAT='%s'
+# Filter out of the history
+HISTIGNORE="&:[bf]g:c:clear:history:exit:q:pwd:* --help"
 
 # Make new shells get the history lines from all previous
 # shells instead of the default "last window closed" history.
 export PROMPT_COMMAND='history -a'
-HISTCONTROL=ignoreboth
-HISTSIZE=10000
-HISTFILESIZE=$HISTSIZE
-HISTTIMEFORMAT='%F %T%t'
-HISTFILE=$HOME/.bash_history
+HISTCONTROL=ignoreboth:erasedups
 
-# Don't add certain commands to the history file.
-export HISTIGNORE="&:[bf]g:c:clear:history:exit:q:pwd:* --help"
-
-# Save all lines in a multiline command in the same history entry
+# Multiline command saved as single history entry
 shopt -s cmdhist
--- .oh-my-shell/zsh/history/default.sh
+++ .oh-my-shell/zsh/history/default.sh
@@ -1,9 +1,31 @@
 # Shell history
+# - History file location
+HISTFILE=$HOME/.zsh_history_permanent
+# - Append command to history
 setopt APPEND_HISTORY # zsh sessions will append their history list to the history file, rather than replace it.
-# multiple parallel zsh sessions will all have the new entries from their history lists added to the history file, in the order that they exit.
-setopt HIST_FIND_NO_DUPS  # When searching for history entries in the line editor, do not display duplicates of a line previously found, even if the duplicates are not contiguous.
-setopt INC_APPEND_HISTORY # works like APPEND_HISTORY except that new history lines are added to the $HISTFILE incrementally (as soon as they are entered), rather than waiting until the shell exits.
-setopt SHARE_HISTORY      # both imports new commands from the history file, and also causes your typed commands to be appended to the history file
-HISTSIZE=10000
+# - Max entries in history in RAM
+HISTSIZE=9000000
+# - Max entries in history on disk
 SAVEHIST=$HISTSIZE
-HISTFILE=$HOME/.zsh_history
+# History record timestamp per command
+setopt EXTENDED_HISTORY
+# Filter out of the history
+HISTORY_IGNORE="&|([bf]g|clear|history|exit|pwd|* --help)"
+# Write the history file immediately
+setopt INC_APPEND_HISTORY
+# Do not display a line previously found.
+setopt HIST_FIND_NO_DUPS         
+# both imports new commands from the history file, and also causes your typed commands to be appended to the history file
+setopt SHARE_HISTORY
+# Expire duplicate entries first when trimming history
+setopt HIST_EXPIRE_DUPS_FIRST
+# Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_DUPS 
+# Delete old recorded entry if new entry is a duplicate.
+setopt HIST_IGNORE_ALL_DUPS
+# Don't record an entry starting with a space.
+setopt HIST_IGNORE_SPACE         
+# Don't write duplicate entries in the history file.
+setopt HIST_SAVE_NO_DUPS         
+# Remove superfluous blanks before recording entry.
+setopt HIST_REDUCE_BLANKS        
