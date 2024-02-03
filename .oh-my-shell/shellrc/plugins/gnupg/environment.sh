701a2e49731ade6a819d29414dd74b91984df6d5
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-03 23:38:11 +0800

    shellrc: export TTY variable when present in zsh

--- .oh-my-shell/shellrc/plugins/gnupg/environment.sh
+++ .oh-my-shell/shellrc/plugins/gnupg/environment.sh
@@ -1,4 +1,11 @@
 # gpg 2.1.0+ requires GPG_TTY to
 #  - hide "Inappropriate ioctl for device" messages
-GPG_TTY=$(tty)
+
+if is_zsh;
+then
+    # https://gist.github.com/mcattarinussi/834fc4b641ff4572018d0c665e5a94d3?permalink_comment_id=4382229#gistcomment-4382229
+    GPG_TTY="${TTY:-"$(tty)"}"
+else
+    GPG_TTY=$(tty)
+fi
 export GPG_TTY
