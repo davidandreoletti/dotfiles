9078e60156c2d69c1d9f3d377efb58961e33d8ea
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-03 23:10:46 +0800

    shellrc: added nnn plugin

---
+++ .oh-my-shell/shellrc/plugins/nnn/environment.sh
@@ -0,0 +1,1 @@
+export NNN_PLUG='f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:imgview'
---
+++ .oh-my-shell/shellrc/plugins/nnn/post.sh
@@ -0,0 +1,3 @@
+# Install nnn's plugins
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+
