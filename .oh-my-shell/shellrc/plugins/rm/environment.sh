bbd3eeea5dd294b0d0262f8e042f7549309bbad7
David Andreoletti <david@andreoletti.net>
2024-02-07 01:50:22 +0800

    shellrc: Create graveyard dir when missing

--- .oh-my-shell/shellrc/plugins/rm/environment.sh
+++ .oh-my-shell/shellrc/plugins/rm/environment.sh
@@ -2,6 +2,6 @@ if is_macos; then
     RM_GRAVEYARD="$HOME/.Trash"
 elif is_linux; then
     RM_GRAVEYARD="$XDG_DATA_HOME/Trash"
-else
-    RM_GRAVEYARD="/tmp/Trash/$USER"
 fi
+
+! test -d "$dir" && mkdir -p "$RM_GRAVEYARD"
