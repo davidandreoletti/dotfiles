a5ac8cb52bf1010ead4c9854b5e8bccb404bae2a
David Andreoletti <david@andreoletti.net>
2024-02-07 16:47:00 +0800

    shellrc: load oci completions on bash only

--- .oh-my-shell/shellrc/plugins/oraclecloud/completions.sh
+++ .oh-my-shell/shellrc/plugins/oraclecloud/completions.sh
@@ -1,2 +1,2 @@
-# oci may not be installed on all machines, conditionally load oci shell completions
-command_exists oci && { eval "$(_OCI_COMPLETE=source oci)" } >/dev/null 2>&1
+# oci completions supported on bash only
+is_bash && command_exists oci && { source $(homebrew_package_path_prefix "oci-cli")/libexec/lib/python*/site-packages/oci_cli/bin/oci_autocomplete.sh} >/dev/null 2>&1
