3c0d0a9c7b7d3f94cced368499621a91eaa0b1a9
David Andreoletti <david@andreoletti.net>
2024-02-07 18:26:49 +0800

    shellrc: support loading bash-completions on homebrew macOS/Linux

--- .oh-my-shell/bash/completions/default.sh
+++ .oh-my-shell/bash/completions/default.sh
@@ -2,14 +2,8 @@
 # Note: List all completions routine with "complete -p"
 
 # Completion for installed homebrew packages, with completion profile.d support
-HOMEBREW_PROFILED_COMPLETION_FILE=$(homebrew_package_path_prefix "/../etc/profile.d/bash_completion.sh")
-if [[ -r "$HOMEBREW_PROFILED_COMPLETION_FILE" ]]; then
-    dot_if_exists "$HOMEBREW_PROFILED_COMPLETION_FILE"
-else
-    for COMPLETION in "$homebrew_package_path_prefix/../etc/bash_completion.d/"*; do
-        dot_if_exists "$COMPLETION"
-    done
-fi
+HOMEBREW_PROFILED_COMPLETION_FILE=$(homebrew_package_path_prefix "/bash-completion@2/etc/profile.d/bash_completion.sh")
+dot_if_exists "$HOMEBREW_PROFILED_COMPLETION_FILE"
 
 # Completion for installed homebrew packages, without completion profile.d support
 HOMEBREW_FZF_COMPLETION_DIR=$(homebrew_package_path_prefix "/fzf/shell/completion.bash")
