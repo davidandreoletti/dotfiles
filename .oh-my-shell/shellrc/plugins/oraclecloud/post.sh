b4614b168e5dda958cb0b71841712153294de86d
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-08 20:32:07 +0800

    shellrc: fixed saving oci completions symlink

--- .oh-my-shell/shellrc/plugins/oraclecloud/post.sh
+++ .oh-my-shell/shellrc/plugins/oraclecloud/post.sh
@@ -1,2 +1,2 @@
 # oci completions supported on bash only
-is_bash && command_exists oci && { ln -sf $(homebrew_package_path_prefix "oci-cli")/libexec/lib/python*/site-packages/oci_cli/bin/oci_autocomplete.sh "$SHELLRC_COMPLETION_USER_DIR/_oci"} >/dev/null 2>&1
+is_bash && command_exists oci && ln -sf $(homebrew_package_path_prefix "oci-cli")/libexec/lib/python*/site-packages/oci_cli/bin/oci_autocomplete.sh "$SHELLRC_COMPLETION_USER_DIR/_oci" >/dev/null 2>&1
