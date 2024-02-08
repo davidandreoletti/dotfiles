ff46be11a273b3908bace200c70379c718311101
David Andreoletti <david@andreoletti.net>
2024-02-08 21:01:11 +0800

    shellrc: run launchd plugin's post on macOS only

--- .oh-my-shell/shellrc/plugins/launchd/post.sh
+++ .oh-my-shell/shellrc/plugins/launchd/post.sh
@@ -1,32 +1,32 @@
-# FIXME Discard run if not macos
-
 # Install daemons/applications/processes/scripts on macOS(launchctl)
 # https://www.launchd.info
 
-target_dir="$HOME/Library/LaunchAgents"
+if is_macos; then
+    target_dir="$HOME/Library/LaunchAgents"
 
-for job in $HOME/.config/launch/*.plist; do
-    job_file="$(basename $job)"
-    job_name="$(echo $job_file | sed -E 's/(.*?)\..*/\1/')"
+    for job in $HOME/.config/launch/*.plist; do
+        job_file="$(basename $job)"
+        job_name="$(echo $job_file | sed -E 's/(.*?)\..*/\1/')"
 
-    target_job="$target_dir/$job_file"
+        target_job="$target_dir/$job_file"
 
-    if [ ! -d "$target_dir" ]; then
-        # Most likely not on the relevant platfrom
-        continue
-    fi
+        if [ ! -d "$target_dir" ]; then
+            # Most likely not on the relevant platfrom
+            continue
+        fi
 
-    # Link job to user agent location
-    if [ -e "$target_job" ]; then
-        # target job exist and linked to local job file
-        continue
-    fi
+        # Link job to user agent location
+        if [ -e "$target_job" ]; then
+            # target job exist and linked to local job file
+            continue
+        fi
 
-    command ln --symbolic --force "$job" "$target_job"
+        command ln --symbolic --force "$job" "$target_job"
 
-    # Permanently enable job to launch at login
-    launchctl load "$target_job"
+        # Permanently enable job to launch at login
+        launchctl load "$target_job"
 
-    # Run job immediately
-    launchctl start "$job_name"
-done
+        # Run job immediately
+        launchctl start "$job_name"
+    done
+fi
