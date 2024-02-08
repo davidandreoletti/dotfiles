1019d6e4a6e839322087fe99afde215204fccc74
David Andreoletti <david@andreoletti.net>
2024-02-09 01:34:45 +0800

    shellrc: LaunchAgents with __USER__ in file name or file content now get replaced with $USER

--- .oh-my-shell/shellrc/plugins/launchd/post.sh
+++ .oh-my-shell/shellrc/plugins/launchd/post.sh
@@ -4,8 +4,15 @@
 if is_macos; then
     target_dir="$HOME/Library/LaunchAgents"
 
-    for job in $HOME/.config/launch/*.plist; do
+    launch_dir="$HOME/.config/launch"
+    for job in $launch_dir/*.plist; do
         job_file="$(basename $job)"
+
+        job_file2="$(echo "$job_file" | sed "s/__USER__/$USER/g")"
+        sed "s/__USER__/$USER/g" "$job" > "$launch_dir/$job_file2"
+        job="$launch_dir/${job_file2}"
+        job_file="$job_file2"
+
         job_name="$(echo $job_file | sed -E 's/\.plist//')"
 
         target_job="$target_dir/$job_file"
@@ -20,11 +27,13 @@ if is_macos; then
             # target job exist and linked to local job file
             :
         else
-            command ln --symbolic --force "$job" "$target_job"
+            # use short options for macOS's ln
+            command ln -s -f "$job" "$target_job"
         fi
 
         # Verify job is well formed
-        if ! plutil -lint "$target_job"; then
+        # - plutil requries a file rather than symlink
+        if ! plutil -lint "$(realpath $target_job)" >/dev/null 2>&1 ; then
             break
         fi
 
