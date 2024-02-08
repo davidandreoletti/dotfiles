8859a29a89f490ef689a40a63fcb90920e7589c2
David Andreoletti <david@andreoletti.net>
2024-02-08 21:16:25 +0800

    shellrc: fixed extracting launch job name from job file name

--- .oh-my-shell/shellrc/plugins/launchd/post.sh
+++ .oh-my-shell/shellrc/plugins/launchd/post.sh
@@ -6,27 +6,32 @@ if is_macos; then
 
     for job in $HOME/.config/launch/*.plist; do
         job_file="$(basename $job)"
-        job_name="$(echo $job_file | sed -E 's/(.*?)\..*/\1/')"
+        job_name="$(echo $job_file | sed -E 's/\.plist//')"
 
         target_job="$target_dir/$job_file"
 
-        if [ ! -d "$target_dir" ]; then
+        if ! test -d "$target_dir"; then
             # Most likely not on the relevant platfrom
             continue
         fi
 
         # Link job to user agent location
-        if [ -e "$target_job" ]; then
+        if test -e "$target_job"; then
             # target job exist and linked to local job file
-            continue
+            :
+        else
+            command ln --symbolic --force "$job" "$target_job"
         fi
 
-        command ln --symbolic --force "$job" "$target_job"
+        # Permanently enable job to launch at login, even if disabled
+        launchctl load -w "$target_job"
 
-        # Permanently enable job to launch at login
-        launchctl load "$target_job"
+        # Load job, even if disabled
+        launchctl load -F "$target_job"
 
         # Run job immediately
         launchctl start "$job_name"
+
+        launchctl list 
     done
 fi
