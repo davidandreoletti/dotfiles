912c3f21014a1ffbfd31455f5e1433e05474f26b
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-09 14:10:15 +0800

    shellrc: launchd accepts optional plist file and duration

--- .oh-my-shell/shellrc/plugins/launchd/post.sh
+++ .oh-my-shell/shellrc/plugins/launchd/post.sh
@@ -1,18 +1,42 @@
 # Install daemons/applications/processes/scripts on macOS(launchctl)
 # https://www.launchd.info
 
+set -x
+
+if test -n "$PLUGIN_LAUNCHD_POST_PLIST"; then
+    PLISTS="$PLUGIN_LAUNCHD_POST_PLIST"
+else
+    PLISTS="*.plist"
+fi
+
+if test -n "$PLUGIN_LAUNCHD_POST_WAIT_DURATION"; then
+    WAIT_DURATION="$PLUGIN_LAUNCHD_POST_WAIT_DURATION"
+else
+    WAIT_DURATION="0"
+fi
+
 if is_macos; then
     target_dir="$HOME/Library/LaunchAgents"
-
     launch_dir="$HOME/.config/launch"
-    for job in $launch_dir/*.plist; do
+
+    for job in $(find "$launch_dir/" -maxdepth 1 -name "$PLISTS"); do
         job_file="$(basename $job)"
 
+        # Generate file name for template based jobs
         job_file2="$(echo "$job_file" | sed "s/__USER__/$USER/g")"
-        job_new="$launch_dir/$job_file2"
-        sed "s/__USER__/$USER/g" "$job" > "$job_new"
-        job="$job_new"
-        job_file="$job_file2"
+
+        # Check if template based job
+        test "$job_file" != "$job_file2";
+        templated=$?
+
+        # Fill template
+        if test $templated -eq 0; then
+            # Template launchd job
+            job_new="$launch_dir/$job_file2"
+            sed "s/__USER__/$USER/g" "$job" > "$job_new"
+            job="$job_new"
+            job_file="$job_file2"
+        fi
 
         job_name="$(echo $job_file | sed -E 's/\.plist//')"
         target_job="$target_dir/$job_file"
@@ -24,20 +48,26 @@ if is_macos; then
 
         # Link job to user agent location
         if test -e "$target_job"; then
-            # target job exist and linked to local job file
+            # target job exist 
             :
+            # Job exist but definition possibly outdated. 
+            # - Consider not waiting at all
+            WAIT_DURATION=0
         else
             # use short options for macOS's ln
             command rsync "$job" "$target_job"
         fi
 
-        for j in "$target_job";
-        do
-            if ! test -s "$j"; then
-                echo "warn:launchd $j is missing OR empty"
-                continue
-            fi
-        done
+        # Remove template from local dir
+        if test $templated -eq 0; then
+            command rm "$job"
+        fi
+
+        # Verify target_job is not empty for some random reason
+        if ! test -s "$target_job"; then
+            echo "warn:launchd $target_job is missing OR empty"
+            continue
+        fi
 
         # Verify job is well formed
         # - plutil requries a file rather than symlink
@@ -53,12 +83,20 @@ if is_macos; then
         fi
         launchctl bootstrap gui/$(id -u) "$target_job"
 
-        # Run job immediately
-        launchctl start "$job_name"
+        until false; do
+            # Run job immediately
+            launchctl start "$job_name"
 
-        # Check job is launched
-        if ! launchctl list | grep "$job_name" >/dev/null 2>&1; then
-            echo "warn:launchd $job_name not loaded/started"
-        fi
+            # Wait for job (in seconds)
+            command sleep ${WAIT_DURATION}
+
+            # Check job is launched
+            if ! launchctl list | grep "$job_name" >/dev/null 2>&1; then
+                echo "warn:launchd $job_name not loaded/started"
+            else
+                break
+            fi
+        done
     done
 fi
+set +x
