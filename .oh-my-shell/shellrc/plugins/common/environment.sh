8bcf6f5a4ac68cc6c7e460894d7fc48b832ed207
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-09 14:10:46 +0800

    shellrc: common plugin loads specific plist files on macOS

--- .oh-my-shell/shellrc/plugins/common/environment.sh
+++ .oh-my-shell/shellrc/plugins/common/environment.sh
@@ -5,17 +5,23 @@ if is_linux; then
     export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
     export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
     export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+elif is_macos; then
+    PLUGIN_LAUNCHD_POST_PLIST="org.__USER__.xdg.environment.plist"
+    PLUGIN_LAUNCHD_POST_WAIT_DURATION="1"
+    . $SHELLRC_PLUGINS_DIR/launchd/post.sh
+    unset -v "PLUGIN_LAUNCHD_POST_PLIST"
+    unset -v "PLUGIN_LAUNCHD_POST_WAIT_DURATION"
 fi
 
 # Create missing XDG folders
 if is_macos; then
-    # Keep synced with .config/launch/org.david.environment.plist's XDG_xxxx var
+    # Keep synced with .config/launch/org.__USER__.environment.plist's XDG_xxxx var
     for xdg in "XDG_CONFIG_HOME" "XDG_CACHE_HOME" "XDG_DATA_HOME" "XDG_STATE_HOME"; do
         xdg_path="$(eval "echo \"\$$xdg\"")"
 
         if test -z "$xdg_path";
         then
-            echo "$xdg is not defined. Forgot to load ~/Library/LaunchAgents/or.$USER.xdg.environment.plist ?"
+            echo "$xdg is not defined. Forgot to load ~/Library/LaunchAgents/org.$USER.xdg.environment.plist ?"
             echo "Existing in 15s. Press Ctrl-C to cancel"
             sleep 15 && exit 1
         else
