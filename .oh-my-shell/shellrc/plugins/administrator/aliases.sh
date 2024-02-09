9c2d7f75107cc4f863e7fdff3049f16c4e97eb75
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-09 15:49:43 +0800

    shellrc: specify administrator_shell_open mode when invoking the alias by the same name

--- .oh-my-shell/shellrc/plugins/administrator/aliases.sh
+++ .oh-my-shell/shellrc/plugins/administrator/aliases.sh
@@ -1,4 +1,4 @@
 # % admin, shell
 # # Open shell session as administrator user
 # ; usage: administrator_open_shell
-alias administrator_open_shell='sudo su - administrator'
+alias administrator_open_shell="sudo su - administrator -c \"export ADMINISTRATOR_SHELL_OPEN='yes'; $SHELL -i -l \""
