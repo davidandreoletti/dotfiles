ec6232463593998bad91ae063edd5937531eb677
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-02-18 22:00:14 +0800

    shellrc: add space to archive related aliases

--- .oh-my-shell/shellrc/plugins/archive/aliases.sh
+++ .oh-my-shell/shellrc/plugins/archive/aliases.sh
@@ -6,26 +6,26 @@ alias archive_compress='ouch compress'
 # % archive, decompress
 # # Uncompress nearly any archive
 # ; usage: archive_uncompress <archive_name>
-alias archive_uncompress='unp'
+alias archive_uncompress='unp '
 
 # % archive, compress, individually
 # # Archive as zip file each root level folder in the specified path
 # ; usage: archive_each_root_folder_separatidely <root_folder>
-alias archive_each_root_folder_separatidely='f_archiveEachFolderSeparatedely'
+alias archive_each_root_folder_separatidely='f_archiveEachFolderSeparatedely '
 
-# % archive, compress, folder, tar.zstd
+# % archive, compress, folder, tar.zst
 # # Archive as tar.gz the specified folder
 # ; Usage: archive_folder <folder_name>
-alias archive_folder='tar -cafv archive.tar.zstd '
+alias archive_folder='tar -cafv archive.tar.zst '
 
 # % archive, compress, send, network
 # # Send a directory over the network fast
 # # u companion app: archive_receive_from
 # ; usage: archive_send_to <destination_ip> <destination_port>
-alias archive_send_to='f_archiveSendToNetwork'
+alias archive_send_to='f_archiveSendToNetwork '
 
 # % archive, decompress, receive, network
 # # Receive a directory over the network fast
 # # uompanion app: archive_send_to
 # ; usage: archive_receive_from <local_port>
-alias archive_receive_from='f_archiveReceiveFromNetwork'
+alias archive_receive_from='f_archiveReceiveFromNetwork '
