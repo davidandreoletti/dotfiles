7eeb44f0c3a5987960cec1f48bfb12c858b3663a
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-03-04 23:22:02 +0800

    bootstrap: removed missing script

--- install/bootstrap_apps.sh
+++ install/bootstrap_apps.sh
@@ -1,4 +1,3 @@
-source "${BOOSTRAP_DIR}/bootstrap_apps_os.sh"
 source "${BOOSTRAP_DIR}/bootstrap_apps_cli.sh"
 source "${BOOSTRAP_DIR}/bootstrap_apps_gui.sh"
 source "${BOOSTRAP_DIR}/bootstrap_apps_desktop_environment.sh"
