4da0f70fd154471e6ac6f88a6b056c90c97eacca
David Andreoletti <david.andreoletti.dev@gmail.com>
2024-03-04 23:23:08 +0800

    bootstrap: use fedora native transmission package

--- install/bootstrap_apps_gui.sh
+++ install/bootstrap_apps_gui.sh
@@ -86,8 +86,6 @@ if is_profile_admin_or_similar; then
 
     # Anaconda
     is_macos   &&  homebrew_brew_cask_install              "miniconda"
-    is_macos   &&  homebrew_brew_cask_install              "transmission"
-    is_fedora  &&  fedora_flatpak_flathub_install          "com.transmissionbt.Transmission"
     is_macos   &&  homebrew_brew_cask_install              "dropbox"
     is_fedora  &&  fedora_flatpak_flathub_install          "com.dropbox.Client"
     is_macos   &&  homebrew_brew_cask_install              "cyberduck"
@@ -114,6 +112,10 @@ if is_profile_admin_or_similar; then
     # Books
     is_macos   &&  homebrew_brew_cask_install              "calibre"
     is_fedora  &&  fedora_dnf_install                      "calibre"
+
+    # Torrent
+    is_macos   &&  homebrew_brew_cask_install              "transmission"
+    is_fedora  &&  fedora_dnf_install                      "transmission"
 
     is_macos   &&  homebrew_brew_cask_install              "grandperspective"   # Disk usage GUI
 
