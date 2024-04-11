2d1f22dea0e153f2c957bf972e414144c8c55ccf
David Andreoletti <david@andreoletti.net>
2024-04-11 23:00:55 +0800

    shellrc: added ~/.local/bin to path for pipx

--- .oh-my-shell/shellrc/plugins/pipx/environment.sh
+++ .oh-my-shell/shellrc/plugins/pipx/environment.sh
@@ -2,7 +2,7 @@
 # - macos: 'pipx environment' command output the follow but is slow. Appending paths hardcoded path instead
 #          PIPX_BIN_DIR=/Users/davidis/.local/bin
 #          PIPX_MAN_DIR=/Users/davidis/.local/share/man
-if is_macos; then
+if is_macos || is_linux; then
     path_prepend "$HOME/.local/bin"
     manpath_prepend "$HOME/.local/share/man"
 fi
