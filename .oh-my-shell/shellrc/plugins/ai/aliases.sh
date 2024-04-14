eb4650233e0ac20c09d768e07b34c6deb23f3772
David Andreoletti <david@andreoletti.net>
2024-04-14 16:32:15 +0800

    shellrc: moved ai plugin to dotfiles-private

--- .oh-my-shell/shellrc/plugins/ai/aliases.sh
+++
@@ -1,5 +0,0 @@
-# % ai, code, default
-# # Setup OpenDevin
-# ; usage: ai_opendevin_run <model_name>
-# ; example: ai_opendevin_run "ollama/llama2:13b-chat-q4_K_M"
-alias ai_opendevin_run='f_ai_opendevin_run '
