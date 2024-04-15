db255615a082191bd1a9ce455d82ebd41416d4d6
David Andreoletti <david@andreoletti.net>
2024-04-15 19:13:27 +0800

    bootstrap: use predefined vcode binary path for bootstrap script when not found automatically

--- install/common/shell/vscode.sh
+++ install/common/shell/vscode.sh
@@ -1,6 +1,19 @@
 vscode_install_extension() {
     local name="$1"
 
-    vscode="$(command which code)"
+    if command which code; then
+        # Find vscode automatically
+        vscode="$(command which code)"
+    else
+        # Find vscode mannually
+                 # Fedora         macOS
+        for p in "/usr/bin/code"  "/usr/local/bin/code"; do
+            if command -v "$p"; then
+                vscode = "$p"
+                break
+            fi
+        done
+    fi
+
     $vscode --install-extension "$name"
 }
