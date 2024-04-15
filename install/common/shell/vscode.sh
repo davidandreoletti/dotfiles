d6b88796a371429852765820f31b7557168ef921
David Andreoletti <david@andreoletti.net>
2024-04-15 19:30:12 +0800

    bootstrap: Code cleanup

--- install/common/shell/vscode.sh
+++ install/common/shell/vscode.sh
@@ -6,7 +6,7 @@ vscode_install_extension() {
         vscode="$(command which code)"
     else
         # Find vscode mannually
-                 # Fedora         macOS
+        # OS path:Fedora         macOS
         for p in "/usr/bin/code"  "/usr/local/bin/code"; do
             if command -v "$p"; then
                 vscode = "$p"
