919d3c93811f1becfada4a1a95a8b402090f853e
David Andreoletti <david@andreoletti.net>
2024-04-19 20:54:04 +0800

    bootstrap: support aarch64 CPU arch

--- install/common/shell/arch.sh
+++ install/common/shell/arch.sh
@@ -5,3 +5,11 @@ is_arch_x86_64() {
         *) return 1 ;;
     esac
 }
+
+is_arch_aarch64() {
+    architecture=""
+    case $(uname -m) in
+        aarch64) return 0 ;;
+        *) return 1 ;;
+    esac
+}
