8852c0420f5f8f298d88f482abd6cd0b582586d1
David Andreoletti <david@andreoletti.net>
2024-05-19 16:56:01 +0800

    conda: add conda binary to path

--- .oh-my-shell/shellrc/plugins/conda/environment.sh
+++ .oh-my-shell/shellrc/plugins/conda/environment.sh
@@ -2,6 +2,10 @@
 # - init code completion, etc
 if is_macos; then
     [ -d "$HOME/.conda" ] || (conda init --quiet $SHELL_NAME)
+elif is_fedora; then
+    # condabin contains only the conda binary
+    path_prepend "/opt/conda/condabin/"
+    [ -d "$HOME/.conda" ] || (conda init --quiet $SHELL_NAME)
 else
     # Linux does not seem to need it per shell instance
     # However sudo conda init --quiet ran during bootstrap_apps.sh
