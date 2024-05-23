172520fae98d23a27628061ecb094cb030de3be3
David Andreoletti <david@andreoletti.net>
2024-05-23 11:47:57 +0800

    bootstrap: fixed variable assignment syntax

--- install/common/shell/vscode.sh
+++ install/common/shell/vscode.sh
@@ -1,11 +1,12 @@
 vscode_install_extension() {
     local name="$1"
 
+    vscode="/path/missing/to/code"
     # Find vscode by os path
-    #        Fedora           macOS
-    for p in "/usr/bin/code"  "/usr/local/bin/code"; do
+    #        Fedora          macOS
+    for p in "/usr/bin/code" "/usr/local/bin/code"; do
         if command -v "$p"; then
-            vscode = "$p"
+            vscode="$p"
             break
         fi
     done
