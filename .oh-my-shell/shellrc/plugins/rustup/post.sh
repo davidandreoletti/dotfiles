cd81f24d67951ccd90bbd530dfccc95483a755a0
David Andreoletti <david@andreoletti.net>
2024-05-23 13:27:54 +0800

    rustup: install+update stable/beta/nightly toolchains

--- .oh-my-shell/shellrc/plugins/rustup/post.sh
+++ .oh-my-shell/shellrc/plugins/rustup/post.sh
@@ -1,5 +1,17 @@
-# rustup is updated via homebrew
-# NO need for 'rustup self update'
+# Initialize rustup-init if never done before
+if test -d "$HOME/.rustup/toolchains"; then
+    # rustup is updated via homebrew
+    # NO need for 'rustup self update'
+    :
 
-# Update rust toolchain
-rustup update 1>/dev/null 2>&1 # silence updates
+    # Update toolchains
+    rustup update 1>/dev/null 2>&1 # silence updates
+else
+    rustup-init -y --verbose
+
+    # Install toolchains 
+    for name in stable beta nightly
+    do
+        rustup toolchain install $name 1>/dev/null 2>&1 
+    done
+fi
