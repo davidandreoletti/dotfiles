c842f7f2f68d3ee1684e0e55a5cfe4363bba4460
David Andreoletti <david@andreoletti.net>
2024-05-23 13:27:22 +0800

    rustup: Install default toolchain IFF rustup is available

--- .oh-my-shell/shellrc/plugins/rustup/environment.sh
+++ .oh-my-shell/shellrc/plugins/rustup/environment.sh
@@ -1,2 +1,4 @@
-# Set default toolchain
-rustup default stable 1>/dev/null 2>&1 # silence
+if command -v "rustup"; then
+    # Set default toolchain
+    rustup default stable 1>/dev/null 2>&1 # silence
+fi
