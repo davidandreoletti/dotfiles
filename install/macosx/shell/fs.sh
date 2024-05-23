67c976b65defb850c8a4b0bb4429156979194db0
David Andreoletti <david@andreoletti.net>
2024-05-23 23:07:57 +0800

    bootstrap: ignore read non 0 exit code due to EOF

--- install/macosx/shell/fs.sh
+++ install/macosx/shell/fs.sh
@@ -29,7 +29,7 @@ MACOSX_SYSTEM_LAUNCH_DAEMON_DIR="/Library/LaunchDaemons"
 # param1: filesystem name
 # usage:"fasname" "plistname" "
 fs_enable_flag_noatime_on_filesystem() {
-    IFS= read -d '' plistfile << "EOF"
+    IFS= read -d '' plistfile << "EOF" || true
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
         "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
