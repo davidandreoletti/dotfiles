a5a4d494cedb104faf804b20291c4f2b474a9cc0
David Andreoletti <david@andreoletti.net>
2024-05-23 22:23:35 +0800

    bootstrap: fixed moving nsmb.conf if non symlinked

--- install/macosx/shell/defaults.sh
+++ install/macosx/shell/defaults.sh
@@ -534,9 +534,9 @@ defaults write com.torusknot.SourceTreeNotMAS analyticsHasAgreed 0
 ###############################################################################
 # Samba Client
 ###############################################################################
-if test -f"$USER/Library/Preferences/nsmb.conf"; then
+if test -f "$USER/Library/Preferences/nsmb.conf"; then
     # Move original file
-    mv $USER/Library/Preferences/{nsmb.conf,nsmb.conf.original}
+    mv $USER/Library/Preferences/nsmb.conf{,.original}
     # New file will be symlinked with stow
 fi
 echo "Done. Note that some of these changes require a logout/restart to take effect."
