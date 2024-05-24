25949d39db56dbef6396a55fb02a28d3ab7e204b
David Andreoletti <david@andreoletti.net>
2024-05-24 10:55:28 +0800

    bootstrap: fixed condition for vim plugins installation

--- install/app/vim/upgrade.sh
+++ install/app/vim/upgrade.sh
@@ -1,6 +1,6 @@
-if command -v vim; then 
+if command -v 'vim'; then 
     command vim +'PlugClean --sync' +qa >/dev/null 2>&1
     command vim +'PlugUpgrade --sync' +qa >/dev/null 2>&1
     command vim +'PlugInstall --sync' +qa >/dev/null 2>&1
     command vim +'PlugUpdate --sync' +qa >/dev/null 2>&1
-if
+fi
