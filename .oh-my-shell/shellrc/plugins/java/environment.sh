d5120b2d36bc9043107af032457124d6dac74f03
David Andreoletti <david@andreoletti.net>
2024-07-24 15:43:07 +0800

    shellrc: use homebrew's openjdk when macOS system java is missing

---
+++ .oh-my-shell/shellrc/plugins/java/environment.sh
@@ -0,0 +1,24 @@
+if is_macos; then
+    FIND_BIN="/usr/bin/find" 
+
+    if /usr/libexec/java_home -F > /dev/null 2>&1; then
+        DEFAULT_JAVA_HOME="$(/usr/libexec/java_home)"
+    else
+        # Since macOS 13+, java is not installed by default
+        # Use homebrew's java as system java
+        DEFAULT_JAVA_HOME="$HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk/Contents/Home"
+    fi
+
+    ########################################################
+    # IMPORTANT: jenv's initialization unsets JAVA_HOME.
+    # User is expected to set it via jenv
+    ########################################################
+    #
+    # 1. Export JAVA_HOME for GUI/shell applications
+    # Java version used: most recent installed
+    launchctl setenv JAVA_HOME "$DEFAULT_JAVA_HOME"
+
+    # 2. Export "JAVA_HOME" for shell applications
+    # Java version used for tooling like android/gradle
+    export JAVA_HOME="$DEFAULT_JAVA_HOME"
+fi
--- .oh-my-shell/shellrc/plugins/java/post.sh
+++ .oh-my-shell/shellrc/plugins/java/post.sh
@@ -1,33 +1,22 @@
 if is_macos; then
+    FIND_BIN="/usr/bin/find" 
+
     #
     # Initialize jenv
     # (easier JAVA_HOME switching)
     #
-    #eval "$(jenv init - --no-rehash)"
+    eval "$(jenv init - --no-rehash)"
 
     # Register JDK with jenv
     (
         # Register already installed JDKs
-        find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\"" \;
-        find "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -xtype d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\"" \;
+        $FIND_BIN "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -type d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\"" \;
+        $FIND_BIN "/Library/Java/JavaVirtualMachines" -mindepth 1 -maxdepth 1 -xtype d -name "*jdk*" -exec bash -c "yes | jenv add \"{}/Contents/Home\"" \;
         # Register perhaps not already registered JDKs
         for directory in $HOMEBREW_CELLAR/openjdk*; do
-            find $directory -depth -maxdepth 4 -type d -regex ".*openjdk.jdk$" -exec bash -c "yes | jenv add {}/Contents/Home" \;
+            $FIND_BIN $directory -depth -maxdepth 4 -type d -regex ".*openjdk.jdk$" -exec bash -c "yes | jenv add {}/Contents/Home" \;
         done
         # Notify jenv to reload shims
         jenv rehash
     ) >/dev/null 2>&1
-
-    ########################################################
-    # IMPORTANT: jenv's initialization unsets JAVA_HOME.
-    # User is expected to set it via jenv
-    ########################################################
-    #
-    # 1. Export JAVA_HOME for GUI/shell applications
-    # Java version used: most recent installed
-    launchctl setenv JAVA_HOME $(/usr/libexec/java_home)
-
-    # 2. Export "JAVA_HOME" for shell applications
-    # Java version used for tooling like android/gradle
-    export JAVA_HOME="$(/usr/libexec/java_home)"
 fi
