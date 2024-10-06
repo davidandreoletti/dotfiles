68002d47d456790dd2f6f89521922e7d84d7b4d4
David Andreoletti <david@andreoletti.net>
2024-10-06 23:00:14 +0800

    shellrc: upgrade nnn plugins when nnn is installed only

--- .oh-my-shell/shellrc/plugins/nnn/post.sh
+++ .oh-my-shell/shellrc/plugins/nnn/post.sh
@@ -1,2 +1,4 @@
-# Install nnn's plugins, overwriting them when needed
-yes 'm' | sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+if command_exists "nnn"; then
+    # Install nnn's plugins, overwriting them when needed
+    yes 'm' | sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+fi
