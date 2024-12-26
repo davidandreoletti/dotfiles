6322f4dcbe890096b121647f22f0e23b44906101
David Andreoletti <david@andreoletti.net>
2024-12-26 15:11:03 +0800

    shellrc: code cleanup

--- .oh-my-shell/shellrc/plugins/rustup/aliases.sh
+++ .oh-my-shell/shellrc/plugins/rustup/aliases.sh
@@ -1,6 +1,5 @@
 # % rustup, run command with nightly toolchain
 # # Run cargo with nightly toolchain
-# ; usage: cargo_nightly <cargo command / args> 
-# ; example: cargo_nightly build --release (same as nightly's cargo build --release) 
+# ; usage: cargo_nightly <cargo command / args>
+# ; example: cargo_nightly build --release (same as nightly's cargo build --release)
 alias cargo_nightly='rustup run nightly cargo '
-
