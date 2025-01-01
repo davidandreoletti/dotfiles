7d9b32edfd4b51168be208d75e0493311f93bfb2
David Andreoletti <david@andreoletti.net>
2025-01-01 14:56:54 +0800

    shellrc: better doc for translate_ aliases

--- .oh-my-shell/shellrc/plugins/translate/aliases.sh
+++ .oh-my-shell/shellrc/plugins/translate/aliases.sh
@@ -2,6 +2,14 @@
 # Usage: trs :fr Hello
 alias translate='trans '
 # Translate text into french
+# Usage: translate_to_fr "this is a house"
+# Usage: translate_to_fr file://file.txt
+# Usage: translate_to_fr https://google.com
+# Usage: translate_to_fr --shell
 alias translate_to_fr='trans :fr'
 # Translate text into english
+# Usage: translate_to_fr "this is a house"
+# Usage: translate_to_fr file://file.txt
+# Usage: translate_to_fr https://google.fr
+# Usage: translate_to_fr --shell
 alias translate_to_gb='trans :en-GB'
