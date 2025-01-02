d7904a6780da72113287d35ca872b76228ffa93c
David Andreoletti <david@andreoletti.net>
2025-01-02 11:57:20 +0800

    shellrc: disable crontab on macOS

--- .oh-my-shell/shellrc/plugins/cron/post.sh
+++ .oh-my-shell/shellrc/plugins/cron/post.sh
@@ -1,2 +1,15 @@
-# On macOs, copy ~/.crontab into the default user crontab at /usr/lib/cron/tabs/some-username
-crontab "$HOME/.crontab"
+# macOs:
+#  - crontab will copy ~/.crontab into the default user crontab at /usr/lib/cron/tabs/some-username
+#    This operation requires permission to access system directories.
+#    Therefore not running crontab
+
+USER_CRONTAB_FILE="$HOME/.crontab"
+if is_linux;
+then
+    if ! crontab -T "$USER_CRONTAB_FILE" > /dev/null 2>&1;
+    then
+        echo "$USER_CRONTAB_FILE is not a valid crontab file."
+    else
+        crontab "$USER_CRONTAB_FILE"
+    fi
+fi
