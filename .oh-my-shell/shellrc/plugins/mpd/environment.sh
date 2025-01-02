6a402cec1f6567dc46c366dcc5f393c1178c56a0
David Andreoletti <david@andreoletti.net>
2025-01-02 12:28:29 +0800

    mpd: refactored mpd config to support linux/macos individually

--- .oh-my-shell/shellrc/plugins/mpd/environment.sh
+++ .oh-my-shell/shellrc/plugins/mpd/environment.sh
@@ -1,4 +1,12 @@
-export MPD_CONF_FILE="$HOME/.config/mpd/mpd.conf"
+if is_linux;
+then
+    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_linux.conf"
+elif is_macos;
+then
+    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_macos.conf"
+else
+    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_missing.conf"
+fi
 
 # Export MPD host/port for mpd clients to communicate with: mpc, ncmpc, pms
 export MPD_PORT=$(command cat $MPD_CONF_FILE | grep "^port" | cut -d\" -f2)
