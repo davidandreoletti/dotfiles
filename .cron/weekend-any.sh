4a7e43b7b65864d7f217c7b2e92c7d55e0b0a4a4
David Andreoletti <david@andreoletti.net>
2025-01-02 10:48:42 +0800

    cron: fixed dotfiles root dir discovery when executing dotfiles related scripts from cron

--- .cron/weekend-any.sh
+++ .cron/weekend-any.sh
@@ -1,3 +1,6 @@
+# Find dotfiles root dir
+DOTFILES_DIR="$(dirname $(dirname $(realpath /home/davidandreoletti/.config/tmux)))"
+
 # Upgrade vim/neovim pluggins
-sh -x $HOME/.dotfiles/install/app/vim/upgrade.sh
-sh -x $HOME/.dotfiles/install/app/neovim/upgrade.sh
+sh -x $DOTFILES_DIR/install/app/vim/upgrade.sh
+sh -x $DOTFILES_DIR/install/app/neovim/upgrade.sh
