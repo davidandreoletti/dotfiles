65814513d02c697e32655a50a52e2357b61cbdb9
David Andreoletti <david@andreoletti.net>
2025-01-08 17:42:42 +0800

    bootstrap: use cargo for some packages

--- install/common/shell/rust.sh
+++ install/common/shell/rust.sh
@@ -1,6 +1,5 @@
 cargo_global_install() {
-    echo "EXPERIMENTAL: do not use yet"
-    exit 1
+    echo "EXPERIMENTAL: use at your own risk"
     cargo=$(which cargo)
     mkdir -p "/usr/local/cargo/bin"
     sudo ${SUDO_OPTIONS} $cargo install --root "/usr/local/cargo/bin" $@
