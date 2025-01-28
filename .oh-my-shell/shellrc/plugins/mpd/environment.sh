cbf5512844aeab367c25c2438a8964ff1aeb910e
David Andreoletti <david@andreoletti.net>
2025-01-28 10:37:04 +0800

    shellrc: fixed retrieving mpd host/port from common config file

--- .oh-my-shell/shellrc/plugins/mpd/environment.sh
+++ .oh-my-shell/shellrc/plugins/mpd/environment.sh
@@ -1,13 +1,15 @@
+export MPD_CONF_DIR="$HOME/.config/mpd"
+export MPD_COMMON_CONF_FILE="$MPD_CONF_DIR/common.conf"
 if is_linux;
 then
-    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_linux.conf"
+    export MPD_CONF_FILE="$MPD_CONF_DIR/mpd_linux.conf"
 elif is_macos;
 then
-    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_macos.conf"
+    export MPD_CONF_FILE="$MPD_CONF_DIR/mpd_macos.conf"
 else
-    export MPD_CONF_FILE="$HOME/.config/mpd/mpd_missing.conf"
+    export MPD_CONF_FILE="$MPD_CONF_DIR/mpd_missing.conf"
 fi
 
 # Export MPD host/port for mpd clients to communicate with: mpc, ncmpc, pms
-export MPD_PORT=$(command cat $MPD_CONF_FILE | grep "^port" | cut -d\" -f2)
-export MPD_HOST=$(command cat $MPD_CONF_FILE | grep "^bind_to_address" | cut -d\" -f2)
+export MPD_PORT=$(command cat $MPD_COMMON_CONF_FILE | grep "^port" | cut -d\" -f2)
+export MPD_HOST=$(command cat $MPD_COMMON_CONF_FILE | grep "^bind_to_address" | cut -d\" -f2)
