3df61ea481ae13a3da15fea018d62228fc0d9424
David Andreoletti <david@andreoletti.net>
2025-02-01 21:58:18 +0800

    shellrc: added libguest_tools plugin

---
+++ .oh-my-shell/shellrc/plugins/libguestfs_tools/aliases.sh
@@ -0,0 +1,5 @@
+# Usage: libguestfs_mount_image "/path/to/image.qcow2" "/dev/sda1" "/mount/point/dir"
+alias libguestfs_mount_image="f_libguestfs_mount_image"
+
+# Usage: libguestfs_unmount_image "/mount/point/dir"
+alias libguestfs_unmount_image="f_libguestfs_unmount_image"
