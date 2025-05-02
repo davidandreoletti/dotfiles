77ad196bf051b5174c57a7656d23084eb5b88191
David Andreoletti <david@andreoletti.net>
2025-05-02 20:51:50 +0800

    shellrc: added timemachine_purge_all_localsnapshots

--- .oh-my-shell/shellrc/plugins/timemachine/aliases.sh
+++ .oh-my-shell/shellrc/plugins/timemachine/aliases.sh
@@ -1,2 +1,5 @@
 # src: https://developer.apple.com/forums/thread/49491
 alias timemachine_show_logs="log show --predicate 'subsystem == "com.apple.TimeMachine"' --info | grep 'upd: (' | cut -c 1-19,140-999"
+
+# Usage: timemachine_purge_all_localsnapshots
+alias timemachine_purge_all_localsnapshots='for d in $(tmutil listlocalsnapshotdates | grep "-"); do sudo tmutil deletelocalsnapshots $d; done'
