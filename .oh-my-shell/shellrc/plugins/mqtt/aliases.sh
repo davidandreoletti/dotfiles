0b91ad505ce88249df17fccfc90c48f93a95dcb3
David Andreoletti <david@andreoletti.net>
2025-05-13 14:08:04 +0800

    shellrc: added alias to log mqtt server messages continuously

--- .oh-my-shell/shellrc/plugins/mqtt/aliases.sh
+++ .oh-my-shell/shellrc/plugins/mqtt/aliases.sh
@@ -3,3 +3,6 @@ alias mqtt_send_anon_message="f_mqtt_send_anonymous_message"
 
 # Usage: tmqtt "mqtt://test.mosquitto.org" "#"
 alias tmqtt='mqttui --broker '
+
+# Usage: tmqtt_logs "mqtt://test.mosquitto.org" "#"
+alias tmqtt_logs='mqttui log --broker '
