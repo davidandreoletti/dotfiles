20d3f92a985344eaf4a388de66a9a9f981b573ff
David Andreoletti <david@andreoletti.net>
2025-10-26 09:24:52 +0800

    shellrc: fixed missing elif in usb function

--- .oh-my-shell/shellrc/plugins/usb/functions.sh
+++ .oh-my-shell/shellrc/plugins/usb/functions.sh
@@ -30,11 +30,11 @@ function f_usbip_start_client_disconnect() {
 function f_usbip_start_server() {
     if is_macos; then
         git clone git@github.com:jiegec/usbip.git usbip.git
-        cd usbip.git
+        pushd usbip.git
         echo "Offering all usb devices"
         env RUST_LOG=info cargo run --example host
-        cd -
-    if is_linux; then
+        popd
+    elif is_linux; then
         # List usb devices
         usbip list -l
         echo "Which device to bind (eg: 1-1.5) ?"
