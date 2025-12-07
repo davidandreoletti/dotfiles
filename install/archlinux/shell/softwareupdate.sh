a638c963ee32add050b4a0e1ba2b2533d7816017
David Andreoletti <david@andreoletti.net>
2025-12-07 15:21:38 +0800

    bootstrap: discard non 0 exit for software update checks

--- install/archlinux/shell/softwareupdate.sh
+++ install/archlinux/shell/softwareupdate.sh
@@ -4,11 +4,20 @@
 
 softwareupdate_list_pending_updates() {
     # Update software database
-    sudo ${SUDO_OPTIONS} pacman -Sy
+    if ! sudo ${SUDO_OPTIONS} pacman -Sy; then
+        # Silence non 0 exit
+        :
+    fi
     # List software update
-    sudo ${SUDO_OPTIONS} pacman -Qu
+    if ! sudo ${SUDO_OPTIONS} pacman -Qu; then
+        # Silence non 0 exit
+        :
+    fi
 }
 
 softwareupdate_updates_install() {
-    sudo ${SUDO_OPTIONS} pacman -Syu --noconfirm
+    if ! sudo ${SUDO_OPTIONS} pacman -Syu --noconfirm; then
+        # Silence non 0 exit
+        :
+    fi
 }
