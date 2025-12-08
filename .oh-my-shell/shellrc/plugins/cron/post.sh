a14d11b4c06d87c37c00d8242bee4a9fff3668c0
David Andreoletti <david@andreoletti.net>
2025-12-08 10:27:31 +0800

    shellrc: code cleanup

--- .oh-my-shell/shellrc/plugins/cron/post.sh
+++ .oh-my-shell/shellrc/plugins/cron/post.sh
@@ -1,10 +1,11 @@
-# macOs:
-#  - crontab will copy ~/.crontab into the default user crontab at /usr/lib/cron/tabs/some-username
-#    This operation requires permission to access system directories.
-#    Therefore not running crontab
-
 USER_CRONTAB_FILE="$HOME/.crontab"
-if is_linux; then
+if is_macos; then
+    # macOs:
+    #  - crontab will copy ~/.crontab into the default user crontab at /usr/lib/cron/tabs/some-username
+    #    This operation requires permission to access system directories.
+    #    Therefore not running crontab
+    :
+elif is_linux; then
     if ! crontab -T "$USER_CRONTAB_FILE" >/dev/null 2>&1; then
         echo "$USER_CRONTAB_FILE is not a valid crontab file."
     else
