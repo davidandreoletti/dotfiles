39781fab69c5150262dcbfc8959780d66febf496
David Andreoletti <david@andreoletti.net>
2025-12-09 12:46:42 +0800

    bootstrap: upgrade mason registry

--- install/app/neovim/upgrade.sh
+++ install/app/neovim/upgrade.sh
@@ -1,4 +1,6 @@
 if command -v nvim; then 
     # sync = install, + clean + update
     command nvim --headless "+Lazy! sync" +qa >/dev/null 2>&1
+    # Update mason's registry
+    command nvim --headless -c "MasonUpdate" +qa >/dev/null 2>&1
 fi
