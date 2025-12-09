28f1d5c9692f9b6dbdc405a61d1d33b7b8cd1b40
David Andreoletti <david@andreoletti.net>
2025-12-09 11:05:01 +0800

    shellrc: bump neovim's python provider version

--- .oh-my-shell/shellrc/plugins/neovim/post.sh
+++ .oh-my-shell/shellrc/plugins/neovim/post.sh
@@ -5,11 +5,14 @@
 #
 
 {
+    # Setup pyenv
+    eval "$(pyenv init - $SHELL_NAME)"
     # Install python version required by neovim
     pyenv install --skip-existing "${_NEOVIM_PYENV_VIRTUALENV_VERSION}"
     # Create python environement for neovim only
     pyenv virtualenv "${_NEOVIM_PYENV_VIRTUALENV_VERSION}" "${_NEOVIM_PYENV_VIRTUALENV_NAME}"
-    # Install neovim python dependencies
+    # Install neovim's python dependencies for vim.provider-python to detect a valid 
+    # python environment
     pyenv exec python -m pip install --upgrade pip
     for pkg in pynvim; do
         pyenv shell "${_NEOVIM_PYENV_VIRTUALENV_NAME}" \
