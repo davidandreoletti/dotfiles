da91b1bbab7ac18a0cfce7fcdbdff03adcb6aaed
David Andreoletti <david@andreoletti.net>
2025-12-09 12:47:14 +0800

    nvim: install mandatory language servers

--- .config/cvim/settings/nvim_mason-lspconfig.lua
+++ .config/cvim/settings/nvim_mason-lspconfig.lua
@@ -1,9 +1,24 @@
 local function config()
     -- Setup lspconfig-mason based on ':h masson-lspconfig automatic-server-setup'
     require('mason-lspconfig').setup({
-         -- LSPs to install is defined in lspconfig's nvim_lspconfig.lua.
-         -- So no need configure these settings here:
-         -- - ensure_installed = {},
+         -- LSPs to configure is defined in lspconfig's nvim_lspconfig.lua.
+         ensure_installed = {
+            "ruff",
+            "pylsp",
+            "vimls",
+            "terraformls",
+            "rust_analyzer",
+            "marksman",
+            "lua_ls",
+            "ltex",
+            "jsonls",
+            "yamlls",
+            "html",
+            "dockerls",
+            "astro",
+            'harper_ls',
+         },
+         -- For INSTALLED LSPs, automatically enabled LSPs (via vim.lsp.enable(...))
          automatic_enable = true
     })
 end
