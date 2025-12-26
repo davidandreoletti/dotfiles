6c8d5909d2f19b6606d218e5d1034b5bf5633b23
David Andreoletti <david@andreoletti.net>
2025-12-26 16:43:17 +0800

    bootstrap: install vimium firefox extension

--- install/browsers/firefox/extensions/install.sh
+++ install/browsers/firefox/extensions/install.sh
@@ -8,8 +8,8 @@ else
     echo "Firefox browser will open multiple window, each asking to install a different add-ons"
     echo "Once all add-ons install, kill the firefox browser instance to resume the installation"
     $FIREFOX_BIN --browser \
-        --new-tab "https://addons.mozilla.org/firefox/downloads/file/1668522/tridactyl-1.14.8-an+fx.xpi" \
-        --new-tab "https://addons.mozilla.org/firefox/downloads/file/1209034/react_developer_tools-3.6.0-fx.xpi"
+        --new-tab "https://addons.mozilla.org/en-US/firefox/addon/vimium-ff" \
+        --new-tab "https://google.com"
     # FIXME: Later, download add ons version, automatically from selenium
     ## https://stackoverflow.com/a/28031137/219728
 fi
