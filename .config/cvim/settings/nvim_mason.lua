85782f4adbb4ef7af5caa185054722f98318dd1c
David Andreoletti <david@andreoletti.net>
2026-01-06 15:30:37 +0800

    nvim: auto upgrade all mason resources at once

--- .config/cvim/settings/nvim_mason.lua
+++ .config/cvim/settings/nvim_mason.lua
@@ -8,6 +8,21 @@ local function config()
         },
     })
 
+    -- Auto update all mason managed resources
+    -- src: https://github.com/mason-org/mason.nvim/discussions/1241#discussioncomment-5701984
+    vim.api.nvim_create_user_command("MasonUpgradeAll", function()
+        local registry = require("mason-registry")
+        registry.refresh()
+        registry.update()
+        local packages = registry.get_all_packages()
+        for _, pkg in ipairs(packages) do
+            if pkg:is_installed() then
+                pkg:install()
+            end
+        end
+        vim.cmd("doautocmd User MasonUpgradeAllComplete")
+    end, { force = true })
+
 end
 
 return {config = config}
