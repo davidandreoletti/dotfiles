606a10a7d5396f94a092e9b4af9c92cae689e308
David Andreoletti <david@andreoletti.net>
2026-01-06 15:30:49 +0800

    cron: update mason managed resources weekly

--- install/app/neovim/upgrade.sh
+++ install/app/neovim/upgrade.sh
@@ -3,4 +3,6 @@ if command -v nvim; then
     command nvim --headless "+Lazy! sync" +qa >/dev/null 2>&1
     # Update mason's registry
     command nvim --headless -c "MasonUpdate" +qa >/dev/null 2>&1
+    # Upgade mason's managed resources
+    command nvim --headless -c "autocmd User MasonUpgradeAllComplete sleep 100m | qall" -c 'MasonUpgradeAll'
 fi
