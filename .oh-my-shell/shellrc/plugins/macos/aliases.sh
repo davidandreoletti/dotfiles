7fe663d7deb8235aacb607531527db45c43d1943
David Andreoletti <david@andreoletti.net>
2026-01-08 10:21:48 +0800

    shellrc: fixed tracking pref change location on macOS

--- .oh-my-shell/shellrc/plugins/macos/aliases.sh
+++ .oh-my-shell/shellrc/plugins/macos/aliases.sh
@@ -1,2 +1,2 @@
 # FIXME: Automated this: https://rderik.com/blog/tracking-where-settings-are-stored-on-macos/
-alias tracking_preferences_changes="sudo fs_usage -f filesys | grep plist;"
+alias tracking_preferences_changes="sudo fs_usage -f filesys | grep plist | grep cfprefsd"
