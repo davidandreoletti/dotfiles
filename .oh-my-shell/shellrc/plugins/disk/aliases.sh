44f8bef2982e82eacba3a8bc2c1e2e4d8f39f3bd
David Andreoletti <david@andreoletti.net>
2026-06-21 11:57:07 +0800

    shellrc: added tdisk_usage_io alias

--- .oh-my-shell/shellrc/plugins/disk/aliases.sh
+++ .oh-my-shell/shellrc/plugins/disk/aliases.sh
@@ -1,7 +1,14 @@
+# Disk storage usage analyzer
 if command_exists 'dua'; then
-    # Disk usage analyzer
     # Usage: tdisk_usage_analyzer /path/fo/folder
     alias tdisk_usage_analyzer='dua i '
 elif command_exists 'ncdu'; then
     alias tdisk_usage_analyzer='ncdu '
+fi
+
+# Disk I/O usage
+if is_linux; then
+    alias tdisk_usage_io='iostat -x 1 10 '
+elif is_macos; then
+    alias tdisk_usage_io='iostat -c 10 -w 1'
 fi
