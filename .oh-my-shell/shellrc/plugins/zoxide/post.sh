77d422bcfca2915a1c041b35388b774ed6d4ea0c
David Andreoletti <david@andreoletti.net>
2026-09-14 15:13:03 +0800

    shellrc: import z file into zoxide when present

--- .oh-my-shell/shellrc/plugins/zoxide/post.sh
+++ .oh-my-shell/shellrc/plugins/zoxide/post.sh
@@ -3,4 +3,7 @@ if test -n "${_Z_DATA}"; then
     echo "Cannot import ${_Z_DATA} into zoxide at custom path is not supported"
     #zoxide import z --merge "${_Z_DATA}"
 fi
-zoxide import z --merge # expected ~/.z
+
+if test -f "$HOME/.z"; then
+    zoxide import z --merge # expected ~/.z
+fi
